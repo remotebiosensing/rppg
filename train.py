20aab1bbb86d9b95740367b08e5211100d0bbec9
spicyyeol <spicyyeol@gmail.com>
2021-03-24 12:27:50 +0900

    add tensorboard feature for confirm mask and model graph

--- train.py
+++ train.py
@@ -5,9 +5,10 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
+import torchvision
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
+import torch.nn.functional as F
 writer = SummaryWriter()
 
 transform = transforms.Compose([transforms.ToTensor()])
@@ -15,8 +16,8 @@ dataset = bvpdataset.bvpdataset(
     data_path="subject_test.npz",
     transform=transform)
 train_set, val_set = torch.utils.data.random_split(dataset, [int(len(dataset)*0.8),int(len(dataset)*0.2+1)],generator=torch.Generator().manual_seed(1))
-train_loader = DataLoader(train_set, batch_size=128, shuffle=True)
-val_loader = DataLoader(val_set, batch_size=128, shuffle=True)
+train_loader = DataLoader(train_set, batch_size=128, shuffle=False)
+val_loader = DataLoader(val_set, batch_size=128, shuffle=False)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -35,10 +36,27 @@ Adadelta = optim.Adadelta(model.parameters(), lr=1)
 for epoch in range(1000000):
     print("==="+str(epoch))
     running_loss = 0.0
-    for i_batch, (avg, mot, lab) in enumerate(train_loader):
+    # for i_batch, (avg, mot, lab) in enumerate(train_loader):
+    for i_batch, (mot,avg, lab) in enumerate(train_loader):
         Adadelta.zero_grad()
         avg, mot, lab = avg.to(device), mot.to(device), lab.to(device)
+
+        if i_batch is 0 and epoch is 0 :
+            #plt.figure(figsize=[100,100])
+           # plt.imshow()
+            writer.add_graph(model,(avg,mot))
+            images = F.interpolate(avg[:10],128)
+            img_grid = torchvision.utils.make_grid(images,nrow=10)
+            writer.add_image('avg',img_grid)
+            images = F.interpolate(mot[:10], 128)
+            mot_grid = torchvision.utils.make_grid(images,nrow=10)
+            writer.add_image('mot',mot_grid)
+
         output = model(avg, mot)
+        if i_batch is 0:
+            mask1,mask2 = model.appearance_model(avg)
+            writer.add_image('mask1', mask1[0],epoch)
+            writer.add_image('mask2',mask2[0],epoch)
         loss = MSEloss(output, lab)
         # if torch.isnan(loss):
         #     continue
