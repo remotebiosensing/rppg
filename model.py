4716f3c17b3735c78af66ec4a53faa88c17cf7e5
jinsoogod <jklim57@naver.com>
2021-03-26 19:19:28 +0900

    parser merge error reset

--- model.py
+++ model.py
@@ -1,52 +1,58 @@
 import torch
 
+
 class appearnce_block(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
-        #layer 1
-        self.a_conv1 = torch.nn.Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=kernel_size,stride=1,padding=1)
+        # layer 1
+        self.a_conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                       stride=1, padding=1)
         self.a_batch1 = torch.nn.BatchNorm2d(self.a_conv1.out_channels)
         # layer 2
-        self.a_conv2 = torch.nn.Conv2d(in_channels=self.a_conv1.out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=1)
+        self.a_conv2 = torch.nn.Conv2d(in_channels=self.a_conv1.out_channels, out_channels=out_channels,
+                                       kernel_size=kernel_size, stride=1, padding=1)
         self.a_batch2 = torch.nn.BatchNorm2d(self.a_conv2.out_channels)
 
-    def forward(self,inputs):
-        #layer 1
+    def forward(self, inputs):
+        # layer 1
         a = self.a_conv1(inputs)
         a = self.a_batch1(a)
         a = torch.tanh(a)
-        #layer 2
+        # layer 2
         a = self.a_conv2(a)
         a = self.a_batch2(a)
         a = torch.tanh(a)
 
         return a
 
+
 class attention_block(torch.nn.Module):
-    def __init__(self,in_channels):
+    def __init__(self, in_channels):
         super().__init__()
-        self.attention = torch.nn.Conv2d(in_channels,1,kernel_size=1,padding=0)
+        self.attention = torch.nn.Conv2d(in_channels, 1, kernel_size=1, padding=0)
+
     def forward(self, input):
         mask = self.attention(input)
         mask = torch.sigmoid(mask)
         B, _, H, W = input.shape
-        norm = 2 * torch.norm(mask, p=1, dim=(1,2,3))
-        norm = norm.reshape(B,1,1,1)
-        mask = torch.div(mask*H*W, norm)
+        norm = 2 * torch.norm(mask, p=1, dim=(1, 2, 3))
+        norm = norm.reshape(B, 1, 1, 1)
+        mask = torch.div(mask * H * W, norm)
         return mask
 
-class appearance_model(torch.nn.Module):
-    def __init__(self,in_channels,kernel_size,out_channels):
-        #1st app_block
-        super().__init__()
-        self.a_block1 = appearnce_block(in_channels,out_channels,kernel_size)
-        self.a_dropout2 = torch.nn.Dropout2d(0.5)
-        self.a_avg2 = torch.nn.AvgPool2d(kernel_size=2,stride=2)
-        self.a_mask3 = attention_block(out_channels)
-        self.a_block4 = appearnce_block(out_channels,out_channels*2,kernel_size)
-        self.a_mask6 = attention_block(out_channels*2)
 
-    def forward(self,inputs):
+class appearance_model(torch.nn.Module):
+    def __init__(self, in_channels, kernel_size, out_channels):
+        # 1st app_block
+        super().__init__()
+        self.a_block1 = appearnce_block(in_channels, out_channels, kernel_size)
+        self.a_dropout2 = torch.nn.Dropout2d(0.5)
+        self.a_avg2 = torch.nn.AvgPool2d(kernel_size=2, stride=2)
+        self.a_mask3 = attention_block(out_channels)
+        self.a_block4 = appearnce_block(out_channels, out_channels * 2, kernel_size)
+        self.a_mask6 = attention_block(out_channels * 2)
+
+    def forward(self, inputs):
         a = self.a_block1(inputs)
         mask1 = self.a_mask3(a)
         a = self.a_dropout2(a)
@@ -55,61 +61,69 @@ class appearance_model(torch.nn.Module):
         mask2 = self.a_mask6(a)
         return mask1, mask2
 
+
 class motion_block(torch.nn.Module):
-    def __init__(self,in_channels, out_channels, kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
-        self.m_conv1 = torch.nn.Conv2d(in_channels,out_channels,kernel_size,padding = 1)
+        self.m_conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size, padding=1)
         self.m_batch1 = torch.nn.BatchNorm2d(out_channels)
-        self.m_conv2 = torch.nn.Conv2d(out_channels,out_channels,kernel_size,padding=1)
+        self.m_conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size, padding=1)
         self.m_batch2 = torch.nn.BatchNorm2d(out_channels)
         self.m_drop3 = torch.nn.Dropout2d(0.5)
-        self.m_avg3 = torch.nn.AvgPool2d(kernel_size=2,stride=2,padding=0)
-    def forward(self,inputs,mask):
+        self.m_avg3 = torch.nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+
+    def forward(self, inputs, mask):
         m = self.m_conv1(inputs)
         m = self.m_batch1(m)
         m = torch.tanh(m)
 
         m = self.m_conv2(m)
         m = self.m_batch2(m)
-        m = torch.mul(m,mask)
+        m = torch.mul(m, mask)
         m = torch.tanh(m)
 
         m = self.m_drop3(m)
         m = self.m_avg3(m)
         return m
 
+
 class motion_model(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
-        self.m_block1 = motion_block(in_channels,out_channels,kernel_size)
-        self.m_block2 = motion_block(out_channels,out_channels*2,kernel_size)
-    def forward(self,inputs,mask1,mask2):
-        m =self.m_block1(inputs,mask1)
-        m = self.m_block2(m,mask2)
+        self.m_block1 = motion_block(in_channels, out_channels, kernel_size)
+        self.m_block2 = motion_block(out_channels, out_channels * 2, kernel_size)
+
+    def forward(self, inputs, mask1, mask2):
+        m = self.m_block1(inputs, mask1)
+        m = self.m_block2(m, mask2)
         return m
+
 
 class fc(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.f_drop1 = torch.nn.Dropout2d(0.5)
-        self.f_linear2 = torch.nn.Linear(64*9*9,128)
-        self.f_linear3 = torch.nn.Linear(128,1,bias=True)
-    def forward(self,input):
-        f = torch.flatten(input,start_dim=1)
+        self.f_linear2 = torch.nn.Linear(64 * 9 * 9, 128)
+        self.f_linear3 = torch.nn.Linear(128, 1, bias=True)
+
+    def forward(self, input):
+        f = torch.flatten(input, start_dim=1)
         f = self.f_drop1(f)
         f = torch.tanh(self.f_linear2(f))
         f = self.f_linear3(f)
         return f
 
+
 class DeepPhys(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
-        self.appearance_model = appearance_model(in_channels=in_channels,out_channels=out_channels,kernel_size=kernel_size)
-        self.motion_model = motion_model(in_channels=in_channels,out_channels=out_channels,kernel_size=kernel_size)
+        self.appearance_model = appearance_model(in_channels=in_channels, out_channels=out_channels,
+                                                 kernel_size=kernel_size)
+        self.motion_model = motion_model(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
         self.fc = fc()
 
-    def forward(self,norm_input,motion_input):
-        mask1,mask2 = self.appearance_model(norm_input)
-        out = self.motion_model(motion_input,mask1,mask2)
+    def forward(self, norm_input, motion_input):
+        mask1, mask2 = self.appearance_model(norm_input)
+        out = self.motion_model(motion_input, mask1, mask2)
         out = self.fc(out)
         return out
--- train.py
+++ train.py
@@ -1,91 +1,70 @@
-import model
-import bvpdataset
 import torch
-import torch.optim as optim
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
 import torchvision
-from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 import torchsummary
 
 writer = SummaryWriter()
 
-transform = transforms.Compose([transforms.ToTensor()])
-dataset = bvpdataset.bvpdataset(
-    data_path="subject_test.npz",
-    transform=transform)
-train_set, val_set = torch.utils.data.random_split(dataset, [int(len(dataset)*0.8),int(len(dataset)*0.2+1)],generator=torch.Generator().manual_seed(1))
-train_loader = DataLoader(train_set, batch_size=128, shuffle=False)
-val_loader = DataLoader(val_set, batch_size=128, shuffle=False)
+class train_model:
+    def __init__(self, models, train_loader, val_loader, criterion, optimizers, model_path, num_epochs, device):
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = models
+        self.num_epochs = num_epochs
+        self.model_path = model_path
+        self.optimizers = optimizers
+        self.criterion = criterion
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.device = device
 
-print('Available devices', torch.cuda.device_count())
-print('Current cuda device', torch.cuda.current_device())
-print(torch.cuda.get_device_name(device))
+        self.model.to(device)
+        tmp_valloss = 100
+        torchsummary.summary(self.model, ((3, 36, 36), (3, 36, 36)), )
 
-GPU_NUM = 1
-torch.cuda.set_device(GPU_NUM)
+        for epoch in range(1000000):
+            print("Train : " + str(epoch)+"=======")
+            running_loss = 0.0
+            for i_batch, (avg, mot, lab) in enumerate(self.train_loader):
+                self.optimizers.zero_grad()
+                avg, mot, lab = avg.to(device), mot.to(device), lab.to(device)
 
-tmp_valloss = 100
+                if i_batch is 0 and epoch is 0:
+                    writer.add_graph(self.model, (avg, mot))
+                    images = F.interpolate(avg[:10], 128)
+                    img_grid = torchvision.utils.make_grid(images, nrow=10)
+                    writer.add_image('avg', img_grid)
+                    images = F.interpolate(mot[:10], 128)
+                    mot_grid = torchvision.utils.make_grid(images, nrow=10)
+                    writer.add_image('mot', mot_grid)
 
-model = model.DeepPhys(in_channels=3, out_channels=32, kernel_size=3).to(device)
-torchsummary.summary(model, ((3,36,36),(3,36,36)), )
-MSEloss = torch.nn.MSELoss()
-Adadelta = optim.Adadelta(model.parameters(), lr=1)
-for epoch in range(1000000):
-    print("==="+str(epoch))
-    running_loss = 0.0
-    for i_batch, (avg, mot, lab) in enumerate(train_loader):
-    #for i_batch, (mot,avg, lab) in enumerate(train_loader):
-        Adadelta.zero_grad()
-        avg, mot, lab = avg.to(device), mot.to(device), lab.to(device)
-
-        if i_batch is 0 and epoch is 0 :
-            #plt.figure(figsize=[100,100])
-           # plt.imshow()
-            writer.add_graph(model,(avg,mot))
-            images = F.interpolate(avg[:10],128)
-            img_grid = torchvision.utils.make_grid(images,nrow=10)
-            writer.add_image('avg',img_grid)
-            images = F.interpolate(mot[:10], 128)
-            mot_grid = torchvision.utils.make_grid(images,nrow=10)
-            writer.add_image('mot',mot_grid)
-
-        output = model(avg, mot)
-        if i_batch is 0:
-            mask1,mask2 = model.appearance_model(avg)
-            writer.add_image('mask1', mask1[0],epoch)
-            writer.add_image('mask2',mask2[0],epoch)
-        loss = MSEloss(output, lab)
-        # if torch.isnan(loss):
-        #     continue
-        loss.backward()
-        running_loss += loss.item()
-        Adadelta.step()
-        if i_batch is 0:
-            writer.add_scalar('training loss', running_loss, epoch)
-        # writer.add_scalar('training loss',running_loss / 128 ,epoch * len(train_loader) + i_batch)
-    with torch.no_grad():
-        val_loss = 0.0
-        for k, (avg, mot, lab) in enumerate(val_loader):
-            avg, mot, lab = avg.to(device), mot.to(device), lab.to(device)
-            val_output = model(avg, mot)
-            v_loss = MSEloss(val_output, lab)
-            # if torch.isnan(v_loss):
-            #     continue
-            val_loss += v_loss
-            if k is 0:
-                writer.add_scalar('val loss', v_loss, epoch)
-                if tmp_valloss > val_loss:
-                    checkpoint = { 'Epoch' :epoch,
-                                  'state_dict' : model.state_dict(),
-                                  'optimizer' : Adadelta.state_dict()}
-                    torch.save(checkpoint,'checkpoint.pth')
-                    tmp_valloss = val_loss
-            # writer.add_scalar('val loss', val_loss / 128, epoch * len(val_loader) + i_batch)
-    writer.close()
-print('Finished Training')
+                output = self.model(avg, mot)
+                if i_batch is 0:
+                    mask1, mask2 = self.model.appearance_model(avg)
+                    writer.add_image('mask1', mask1[0], epoch)
+                    writer.add_image('mask2', mask2[0], epoch)
+                loss = criterion(output, lab)
+                loss.backward()
+                running_loss += loss.item()
+                optimizers.step()
+                if i_batch is 0:
+                    writer.add_scalar('training loss', running_loss, epoch)
+                # writer.add_scalar('training loss',running_loss / 128 ,epoch * len(train_loader) + i_batch)
+            with torch.no_grad():
+                val_loss = 0.0
+                for k, (avg, mot, lab) in enumerate(val_loader):
+                    avg, mot, lab = avg.to(device), mot.to(device), lab.to(device)
+                    val_output = self.model(avg, mot)
+                    v_loss = criterion(val_output, lab)
+                    val_loss += v_loss
+                    if k is 0:
+                        writer.add_scalar('val loss', v_loss, epoch)
+                        if tmp_valloss > val_loss:
+                            checkpoint = {'Epoch': epoch,
+                                          'state_dict': self.model.state_dict(),
+                                          'optimizer': self.optimizers.state_dict()}
+                            torch.save(checkpoint, 'checkpoint_train.pth')
+                            tmp_valloss = val_loss
+                    # writer.add_scalar('val loss', val_loss / 128, epoch * len(val_loader) + i_batch)
+            writer.close()
+        print('Finished Training')
