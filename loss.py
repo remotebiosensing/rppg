c1386ed3c0090ac09576692ae9b8c39ee64e1a6c
Daeyeol kim <spicyyeol@gmail.com>
2021-07-22 14:27:58 +0900

    add loss.py

---
+++ loss.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.modules.loss as loss
+
+
+def loss_fn(loss_fn: str = "mse"):
+    if loss_fn == "mse":
+        return loss.MSELoss()
+    elif loss_fn == "L1":
+        return loss.L1Loss()
+    elif loss_fn == "neg_pearson":
+        return NegPearsonLoss()
+    elif loss_fn == "multi_margin":
+        return loss.MultiMarginLoss()
+    elif loss_fn == "bce":
+        return loss.BCELoss()
+    elif loss_fn == "huber":
+        return loss.HuberLoss()
+    elif loss_fn == "cosine_embedding":
+        return loss.CosineEmbeddingLoss()
+    elif loss_fn == "cross_entropy":
+        return loss.CrossEntropyLoss()
+    elif loss_fn == "ctc":
+        return loss.CTCLoss()
+    elif loss_fn == "bce_with_logits":
+        return loss.BCEWithLogitsLoss()
+    elif loss_fn == "gaussian_nll":
+        return loss.GaussianNLLLoss()
+    elif loss_fn == "hinge_embedding":
+        return loss.HingeEmbeddingLoss()
+    elif loss_fn == "KLDiv":
+        return loss.KLDivLoss()
+    elif loss_fn == "margin_ranking":
+        return loss.MarginRankingLoss()
+    elif loss_fn == "multi_label_margin":
+        return loss.MultiLabelMarginLoss()
+    elif loss_fn == "multi_label_soft_margin":
+        return loss.MultiLabelSoftMarginLoss()
+    elif loss_fn == "nll":
+        return loss.NLLLoss()
+    elif loss_fn == "nll2d":
+        return loss.NLLLoss2d()
+    elif loss_fn == "pairwise":
+        return loss.PairwiseDistance()
+    elif loss_fn == "poisson_nll":
+        return loss.PoissonNLLLoss()
+    elif loss_fn == "smooth_l1":
+        return loss.SmoothL1Loss()
+    elif loss_fn == "soft_margin":
+        return loss.SoftMarginLoss()
+    elif loss_fn == "triplet_margin":
+        return loss.TripletMarginLoss()
+    elif loss_fn == "triplet_margin_distance":
+        return loss.TripletMarginWithDistanceLoss()
+    else:
+        return None
+
+
+
+def neg_Pearson_Loss(predictions, targets):
+    '''
+    :param predictions: inference value of trained model
+    :param targets: target label of input data
+    :return: negative pearson loss
+    '''
+    rst = 0
+    for i in range(predictions.shape[0]):
+        sum_x = torch.sum(predictions[i])  # x
+        sum_y = torch.sum(targets[i])  # y
+        sum_xy = torch.sum(predictions[i] * targets[i])  # xy
+        sum_x2 = torch.sum(torch.pow(predictions[i], 2))  # x^2
+        sum_y2 = torch.sum(torch.pow(targets[i], 2))  # y^2
+        N = predictions.shape[1]
+        pearson = (N * sum_xy - sum_x * sum_y) / (
+            torch.sqrt((N * sum_x2 - torch.pow(sum_x, 2)) * (N * sum_y2 - torch.pow(sum_y, 2))))
+
+        rst += 1 - pearson
+
+    rst = rst / predictions.shape[0]
+    return rst
+
+
+class NegPearsonLoss(nn.Module):
+    def __init__(self):
+        super(NegPearsonLoss, self).__init__()
+
+    def forward(self, predictions, targets):
+        return neg_Pearson_Loss(predictions, targets)
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import time
 import datetime
 import json
 
+from loss import loss_fn
 from dataset.dataset_loader import dataset_loader
 from torch.utils.data import DataLoader, random_split
 from utils.dataset_preprocess import preprocessing
 
 with open('params.json') as f:
     jsonObject = json.load(f)
-    __TIME__ = jsonObject.get("time_check")
+    __TIME__ = jsonObject.get("__TIME__")
     params = jsonObject.get("params")
+    hyper_params = jsonObject.get("hyper_params")
 
 '''
     Generate preprocessed data hpy file 
@@ -27,40 +29,47 @@ if False:
 '''
     load dataset before using Torch DataLoader
 '''
-if __TIME__:
-    start_time = time.time()
+if False:
+    if __TIME__:
+        start_time = time.time()
 
-dataset = dataset_loader(save_root_path=params["save_root_path"],
-                         dataset_name=params["dataset_name"],
-                         option="train")
+    dataset = dataset_loader(save_root_path=params["save_root_path"],
+                             dataset_name=params["dataset_name"],
+                             option="train")
 
-train_dataset, validation_dataset = random_split(dataset,
-                                                 [int(np.floor(
-                                                     len(dataset) * params["validation_ratio"])),
-                                                     int(np.ceil(
-                                                         len(dataset) * (1 - params["validation_ratio"])))]
-                                                 )
-if __TIME__:
-    print("load train hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+    train_dataset, validation_dataset = random_split(dataset,
+                                                     [int(np.floor(
+                                                         len(dataset) * params["validation_ratio"])),
+                                                         int(np.ceil(
+                                                             len(dataset) * (1 - params["validation_ratio"])))]
+                                                     )
+    if __TIME__:
+        print("load train hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
-if __TIME__:
-    start_time = time.time()
-test_dataset = dataset_loader(save_root_path=params["save_root_path"],
-                              dataset_name=params["dataset_name"],
-                              option="test")
-if __TIME__:
-    print("load test hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+    if __TIME__:
+        start_time = time.time()
+    test_dataset = dataset_loader(save_root_path=params["save_root_path"],
+                                  dataset_name=params["dataset_name"],
+                                  option="test")
+    if __TIME__:
+        print("load test hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
-'''
-    Call dataloader for iterate dataset
-'''
-if __TIME__:
-    start_time = time.time()
-train_loader = DataLoader(train_dataset, batch_size=params["train_batch_size"],
-                          shuffle=params["train_shuffle"])
-validation_loader = DataLoader(validation_dataset_dataset, batch_size=params["train_batch_size"],
-                               shuffle=params["train_shuffle"])
-test_loader = DataLoader(test_dataset, batch_size=params["test_batch_size"],
-                         shuffle=params["test_shuffle"])
-if __TIME__:
-    print("generate dataloader time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+    '''
+        Call dataloader for iterate dataset
+    '''
+    if __TIME__:
+        start_time = time.time()
+    train_loader = DataLoader(train_dataset, batch_size=params["train_batch_size"],
+                              shuffle=params["train_shuffle"])
+    validation_loader = DataLoader(validation_dataset_dataset, batch_size=params["train_batch_size"],
+                                   shuffle=params["train_shuffle"])
+    test_loader = DataLoader(test_dataset, batch_size=params["test_batch_size"],
+                             shuffle=params["test_shuffle"])
+    if __TIME__:
+        print("generate dataloader time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+
+criterion = loss_fn(hyper_params["loss_fn"])
+if criterion is None:
+    print("use implemented loss functions")
+    print(hyper_params["loss_fn_comment"])
+    raise NotImplementedError("implement a custom function(%s) in loss.py" % hyper_params["loss_fn"])
