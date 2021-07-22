7fc55c6dfd96e222f1dcac9539f8d1169290c65e
Daeyeol kim <spicyyeol@gmail.com>
2021-07-22 10:57:55 +0900

    add json parser
    1. time check option
    2. dataset preprocessing and dataloader option

--- main.py
+++ main.py
@@ -1,25 +1,66 @@
 import time
 import datetime
+import json
+
 from dataset.dataset_loader import dataset_loader
-from torch.utils.data import DataLoader,random_split
+from torch.utils.data import DataLoader, random_split
 from utils.dataset_preprocess import preprocessing
 
+with open('params.json') as f:
+    jsonObject = json.load(f)
+    __TIME__ = jsonObject.get("time_check")
+    params = jsonObject.get("params")
+
+'''
+    Generate preprocessed data hpy file 
+'''
 if False:
+    if __TIME__:
+        start_time = time.time()
+    preprocessing(save_root_path=params["save_root_path"],
+                  data_root_path=params["data_root_path"],
+                  dataset_name=params["dataset_name"],
+                  train_ratio=params["train_ratio"])
+    if __TIME__:
+        print("preprocessing time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+
+'''
+    load dataset before using Torch DataLoader
+'''
+if __TIME__:
     start_time = time.time()
-    preprocessing(save_root_path="/media/hdd1/dy_dataset/",
-                  data_root_path="/media/hdd1/",
-                  dataset_name="UBFC",
-                  train_ratio=0.8)
-    print("preprocessing time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
-start_time = time.time()
-train_dataset = dataset_loader(save_root_path="/media/hdd1/dy_dataset/",
-                               dataset_name="UBFC",
-                               option="train")
-print("load train hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+dataset = dataset_loader(save_root_path=params["save_root_path"],
+                         dataset_name=params["dataset_name"],
+                         option="train")
 
-start_time = time.time()
-test_dataset = dataset_loader(save_root_path="/media/hdd1/dy_dataset/",
-                              dataset_name="UBFC",
+train_dataset, validation_dataset = random_split(dataset,
+                                                 [int(np.floor(
+                                                     len(dataset) * params["validation_ratio"])),
+                                                     int(np.ceil(
+                                                         len(dataset) * (1 - params["validation_ratio"])))]
+                                                 )
+if __TIME__:
+    print("load train hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+
+if __TIME__:
+    start_time = time.time()
+test_dataset = dataset_loader(save_root_path=params["save_root_path"],
+                              dataset_name=params["dataset_name"],
                               option="test")
-print("load test hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+if __TIME__:
+    print("load test hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
+
+'''
+    Call dataloader for iterate dataset
+'''
+if __TIME__:
+    start_time = time.time()
+train_loader = DataLoader(train_dataset, batch_size=params["train_batch_size"],
+                          shuffle=params["train_shuffle"])
+validation_loader = DataLoader(validation_dataset_dataset, batch_size=params["train_batch_size"],
+                               shuffle=params["train_shuffle"])
+test_loader = DataLoader(test_dataset, batch_size=params["test_batch_size"],
+                         shuffle=params["test_shuffle"])
+if __TIME__:
+    print("generate dataloader time \t: ", datetime.timedelta(seconds=time.time() - start_time))
