fb6a2c3d69ede0a8c513a4e8ce454e6f229d7a43
yoojin <yoojin2115@tvstorm.com>
2021-08-04 14:20:39 +0900

    add Meta_preprocessing

--- utils/dataset_preprocess.py
+++ utils/dataset_preprocess.py
@@ -56,6 +56,68 @@ def preprocessing(save_root_path: str = "/media/hdd1/dy_dataset/",
     test_file.close()
 
 
+def Meta_preprocessing(save_root_path: str = "/media/hdd1/dy_dataset/",
+                  model_name: str = "DeepPhys",
+                  data_root_path: str = "/media/hdd1/",
+                  dataset_name: str = "UBFC"):
+    """
+    :param save_root_path: save file destination path
+    :param model_name: select preprocessing method
+    :param data_root_path: data set root path
+    :param dataset_name: data set name(ex. UBFC, COFACE)
+    :param train_ratio: data split [ train ratio : 1 - train ratio]
+    :return:
+    """
+    dataset_root_path = data_root_path + dataset_name
+
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+
+    data_list = [data for data in os.listdir(dataset_root_path) if data.__contains__("subject")]
+
+    process = []
+
+    # multiprocessing
+    for index, data_path in enumerate(data_list):
+        proc = multiprocessing.Process(target=preprocess_Dataset,
+                                       args=(dataset_root_path + "/" + data_path, True, model_name, return_dict))
+        process.append(proc)
+        proc.start()
+
+    for proc in process:
+        proc.join()
+
+    for index, data_path in enumerate(return_dict.keys()):
+        video = return_dict[data_path]['preprocessed_video']
+        label = return_dict[data_path]['preprocessed_label']
+
+        data_len = len(label) //8 # 1개의 데이터를 8개로
+        for i in range(8):
+            n_video = video[data_len * i:data_len * (i + 1)]
+            n_label = label[data_len * i:data_len * (i + 1)]
+
+            dir = save_root_path + model_name + '/'+dataset_name+'/'+data_path
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+            train_file = h5py.File(dir+"/{}_{}.hdf5".format(data_path, i), "w")
+            train_file.create_dataset('preprocessed_video', data=n_video)
+            train_file.create_dataset('preprocessed_label', data=n_label)
+
+            train_file.close()
+
+    #make_meta_filelist
+    folderlist = save_root_path + model_name + '/'+dataset_name +'/'
+    filelist = save_root_path + model_name + '/'+dataset_name+'_filelist/'
+    if not os.path.exists(filelist):
+        os.makedirs(filelist)
+    for i in sorted(os.listdir(folderlist)):
+        for j in sorted(os.listdir(folderlist + i)):
+            file = open(filelist + '{}.txt'.format(i), 'a')
+            file.write(folderlist + i + '/' + j + '\n')  # +''/ground_truth.txt
+            file.close()
+
+
 def preprocess_Dataset(path, flag, model_name, return_dict):
     """
     :param path: dataset path
@@ -67,6 +129,8 @@ def preprocess_Dataset(path, flag, model_name, return_dict):
         rst, preprocessed_video = Deepphys_preprocess_Video(path + "/vid.avi", flag)
     elif model_name == "PhysNet":
         rst, preprocessed_video = PhysNet_preprocess_Video(path + "/vid.avi", flag)
+    elif model_name == "MetaPhys":
+        rst, preprocessed_video = Deepphys_preprocess_Video(path + "/vid.avi", flag)
     if not rst:  # can't detect face
         return
 
@@ -74,6 +138,8 @@ def preprocess_Dataset(path, flag, model_name, return_dict):
         preprocessed_label = Deepphys_preprocess_Label(path + "/ground_truth.txt")
     elif model_name == "PhysNet":
         preprocessed_label = PhysNet_preprocess_Label(path + "/ground_truth.txt")
+    elif model_name == "MetaPhys":
+        preprocessed_label = Deepphys_preprocess_Label(path + "/ground_truth.txt")
 
     return_dict[path.split("/")[-1]] = {'preprocessed_video': preprocessed_video,
                                         'preprocessed_label': preprocessed_label}
