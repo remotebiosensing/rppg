85592842841394f69f9c2098cee256b882d20335
paperchae <forownsake@gmail.com>
2022-08-24 20:38:50 +0900

    revised activation function of model (Tanh -> ReLU)

--- nets/modules/sub_modules/bvp2abp.py
+++ nets/modules/sub_modules/bvp2abp.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from tqdm import tqdm
+import wandb
+
 '''
 nn.Conv1d expects as 3-dimensional input in the shape of [batch_size, channels, seq_len]
 '''
@@ -15,34 +18,39 @@ class bvp2abp(nn.Module):
             nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
                       kernel_size=kernel_size, stride=1, padding=1),
             nn.BatchNorm1d(out_channels),
-            nn.Tanh())
+            nn.ReLU())
 
         # Convolution 3x3 kernel Layer 2 (32@7500 -> 32@7500)
         self.conv_layer2 = torch.nn.Sequential(
             torch.nn.Conv1d(in_channels=out_channels, out_channels=out_channels,
                             kernel_size=kernel_size, stride=1, padding=1),
             nn.BatchNorm1d(out_channels),
-            nn.Tanh())
+            nn.ReLU())
+
+        self.conv_layer3 = torch.nn.Sequential(
+            torch.nn.Conv1d(in_channels=out_channels, out_channels=1,
+                            kernel_size=kernel_size, stride=1, padding=1),
+            nn.BatchNorm1d(1),
+            nn.ReLU())
 
         # Dropout
-        self.a_dropout = torch.nn.Dropout2d(p=0.5)
-        # Average-pooling 2x2 kernel Layer 3 (32@7500 -> 32@3250)
+        self.a_dropout = torch.nn.Dropout1d(p=0.5)
+        # Average-pooling 2x2 kernel Layer 3 (32@7500 -> 32@3750)
         self.avg_pool1 = torch.nn.AvgPool1d(kernel_size=2, stride=2)
-        self.dense = torch.nn.Linear(3750,7500)
-
+        self.dense = torch.nn.Linear(3750, 7500)
 
     def forward(self, ple_input):
-        #print('shape of ple_input :', ple_input.shape)
-        ple_input = torch.reshape(ple_input, (1, 1, len(ple_input)))
-        #print('shape of ple_input :', ple_input.shape)
+        ple_input = torch.reshape(ple_input, (-1, 1, 7500))  # [ batch , channel, size]
+        # print('reshape of ple_input :', ple_input.shape)
         c1 = self.conv_layer1(ple_input)
-        #print('shape of conv1 output :', c1.shape)
+        # print('shape of conv1 output :', c1.shape)
         c2 = self.conv_layer2(c1)
-        #print('shape of conv2 output :', c2.shape)
+        # print('shape of conv2 output :', c2.shape)
         d1 = self.a_dropout(c2)
-        print('shape of dropout output :', d1.shape)
-        p1 = self.avg_pool1(d1)
-        print('shape of avg_pool1 output :', p1.shape)
+        c3 = self.conv_layer3(d1)
+        # print('shape of dropout output :', d1.shape)
+        p1 = self.avg_pool1(c3)
+        # print('shape of avg_pool1 output :', p1.shape)
         out = self.dense(p1)
-        print('shape of dense output :', out.shape)
+        # print('shape of dense output :', out.shape)
         return out
