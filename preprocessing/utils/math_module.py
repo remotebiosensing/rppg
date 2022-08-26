efe7f631118af65efddc9785698d8cf667b4351e
paperchae <forownsake@gmail.com>
2022-08-26 12:28:44 +0900

    changed derivative() -> diff_np() for signal differential

--- preprocessing/utils/math_module.py
+++ preprocessing/utils/math_module.py
@@ -1,17 +1,44 @@
+import torch
+import torch.nn as nn
 import numpy as np
+from tqdm import tqdm
 
-test = np.array([5, 4, 3, 2, 1, 2, 3, 4, 5])
+'''should be done before sig_slicing'''
 
 
 def derivative(input_sig):
+    # print('input_sig :', np.shape(input_sig))
+    # print('input_sig :', input_sig)
     # deriv = np.array(len(input_sig))
     temp = input_sig[1:]
-    temp = np.append(temp, 0)
-    deriv = np.subtract(temp, input_sig)
-    print(temp)
-    print('deriv :', deriv)
+    # print('input_sig[1:]', np.shape(temp))
+    # print('input_sig[1:]', temp)
+    temp = np.append(temp, temp[-1])
+    # print('temp', np.shape(temp))
+    # print('temp', temp)
+    # print('np.append(temp,0)', temp)
+    # deriv = np.subtract(temp, input_sig)
+    deriv = [tempi - inputi for tempi, inputi in zip(temp, input_sig)]
+    # print('deriv :', np.shape(deriv))
+    # print('deriv :', deriv)
     return deriv
 
 
-deri = derivative(test)
-print(deri)
+'''
+np.shape(input_sig) : ndarray(702, 7500)     ex) ple, abp
+'''
+
+
+
+def diff_np(input_sig):
+    diff = []
+    # print('before diff_np :', np.shape(input_sig))
+    for s in input_sig:
+        temp = np.append(s[1:], s[-1]) - s
+        temp[-1] = np.mean(temp[-3:-2])
+        diff.append(temp)
+    diff = np.array(diff)
+    # print('before diff_np :', np.shape(diff))
+    return diff
+
+
