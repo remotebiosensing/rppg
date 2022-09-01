cedd66b3acdcf741a5a4ed75e9e45a287f274032
paperchae <forownsake@gmail.com>
2022-09-01 22:08:45 +0900

    edited test.py to support all the channel cases

--- test.py
+++ test.py
@@ -6,48 +6,88 @@ from matplotlib import pyplot as plt
 import numpy as np
 from preprocessing.utils import math_module
 from scipy import signal
+import json
+
+with open('/home/paperc/PycharmProjects/BPNET/parameter.json') as f:
+    json_data = json.load(f)
+    param = json_data.get("parameters")
+    orders = json_data.get("parameters").get("in_channels")
+    test_orders = json_data.get("parameters").get("test_channels")
+    sampling_rate = json_data.get("parameters").get("sampling_rate")
+
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+order = orders["first"]
+# degree = order - 1
+test_order = test_orders["third"]
+degree = test_order - 1
+# test_len = int(param["down_sample"] / 60) * 2
+test_len = int(param["chunk_size"] / 125) * 2
 
 def model_test():
     root_path = '/home/paperc/PycharmProjects/BPNET/dataset/mimic-database-1.0.0/'
 
-    t_path2 = '055/05500011'
+    # t_path2 = '472/47200003'
+    # t_path2 = '039/03900011'
+    t_path2 = '055/05500100'
     test_record = MIMICdataset.read_record(root_path + t_path2)
-    chunk_size = 10
-    test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_size)
+    print(np.shape(test_record))
+    chunk_num = 10
 
-    ple = []
-    abp = []
+    if degree == 0:
+        test_ple_total, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+    elif degree == 1:
+        test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+        test_ple_first = math_module.diff_np(test_ple)
+        test_ple_total = test_ple_first
+    elif degree == 2:
+        test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+        test_ple_first = math_module.diff_np(test_ple)
+        test_ple_second = math_module.diff_np(test_ple_first)
+        test_ple_total = test_ple_second
+    elif degree == 3:
+        test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+        test_ple_first = math_module.diff_np(test_ple)
 
-    for i in range(len(test_ple)):
-        ple.append(signal.resample(test_ple[i], 1800))
-        abp.append(signal.resample(test_abp[i], 1800))
+        test_ple_total = math_module.diff_channels_aggregator(test_ple, test_ple_first)
+        test_abp = signal.resample(test_abp, int(param["chunk_size"] / 125) * sampling_rate["60"])
+    elif degree == 4:
+        test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+        test_ple_first = math_module.diff_np(test_ple)
+        test_ple_second = math_module.diff_np(test_ple_first)
 
-    test_ple = np.asarray(ple)
-    test_abp = np.asarray(abp)
+        test_ple_total = math_module.diff_channels_aggregator(test_ple, test_ple_second)
+    elif degree == 5:
+        test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+        test_ple_first = math_module.diff_np(test_ple)
+        test_ple_second = math_module.diff_np(test_ple_first)
 
-    test_ple_first = math_module.diff_np(test_ple)
-    test_ple_second = math_module.diff_np(test_ple_first)
+        test_ple_total = math_module.diff_channels_aggregator(test_ple_first, test_ple_second)
+    else:
+        test_ple, test_abp = MIMICdataset.signal_slicing(signals=test_record, chunk_num=chunk_num)
+        test_ple_first = math_module.diff_np(test_ple)
+        test_ple_second = math_module.diff_np(test_ple_first)
 
-    test_total = math_module.diff_channels_aggregator(test_ple, test_ple_first, test_ple_second)
-    print('test_total shape :', np.shape(test_total))
+        test_ple_total = math_module.diff_channels_aggregator(test_ple, test_ple_first, test_ple_second)
 
-    test_dataset = customdataset.CustomDataset(x_data=test_total[0:2], y_data=test_abp[0:2])
-    test_loader = DataLoader(test_dataset, batch_size=7500, shuffle=False)
+    print('test_ple shape :', np.shape(test_ple_total))
+    print('test_abp shape :', np.shape(test_abp))
+
+    test_dataset = customdataset.CustomDataset(x_data=test_ple_total[0:2], y_data=test_abp[0:2])
+    test_loader = DataLoader(test_dataset, batch_size=10, shuffle=False)
 
     dataiter = iter(test_loader)
     seq, labels = dataiter.__next__()
 
-    Model_PATH = '/home/paperc/PycharmProjects/BPNET/weights/'
+    Model_PATH = param["save_path"]
 
-    load_model = torch.load(Model_PATH + 'model_derivative_test.pt')  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
+    # load_model = torch.load(Model_PATH + 'model_' + str(order - 1) + '_NegMAE_baseline.pt')  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
+    load_model = torch.load(Model_PATH + 'model_110_NegMAE_newmodel_temp.pt')  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
     # load_model.load_state_dict(torch.load(Model_PATH + 'model_state_dict.pt'))  # state_dict를 불러 온 후, 모델에 저장
 
     with torch.no_grad():
-        # X_test = mnist_test.test_data.view(len(mnist_test), 1, 28, 28).float().to(device)
         X_test = seq
         Y_test = labels
 
@@ -56,14 +96,8 @@ def model_test():
         print('np.shape(X_test) :', np.shape(X_test))
         print('np.shape(prediction) :', np.shape(prediction))
 
-        prediction = torch.squeeze(prediction).cpu().numpy()[0][:180]
-        Y_test = Y_test.cpu().numpy()[0][:180]
-        # prediction = np.reshape(prediction.cpu().numpy().tolist(), (15000, 1))[:500]
-        # Y_test = Y_test.cpu().numpy().tolist()[:500]
-        # print(prediction[:10])
-        # correct_prediction = torch.argmax(prediction, 1) == Y_test
-        # accuracy = correct_prediction.float().mean()
-        # print('Accuracy :', accuracy.item())
+        prediction = torch.squeeze(prediction).cpu().numpy()[0][30:test_len + 30]
+        Y_test = Y_test.cpu().numpy()[0][30:test_len + 30]
 
         time = np.array(range(len(prediction)))
         fig, ax1 = plt.subplots()
@@ -75,7 +109,7 @@ def model_test():
         ax2.plot(time, Y_test, color="blue")
         ax2.tick_params(axis='y', labelcolor="blue")
 
-        plt.title('RMSE * Neg_Pearson_Loss prediction')
+        plt.title('F + F\' MAE * Neg_Pearson_Loss prediction')
         plt.show()
 
 
