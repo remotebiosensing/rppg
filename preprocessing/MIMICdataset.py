c68e166e10c90eb662a9b0bbf25f1ffec99639ca
paperchae <forownsake@gmail.com>
2022-09-01 22:06:16 +0900

    added TODO / changed data_aggregator() to take degree for selecting diff_channel_aggregator / changed to save preprocessed_data using h5py

--- preprocessing/MIMICdataset.py
+++ preprocessing/MIMICdataset.py
@@ -5,7 +5,15 @@ import numpy as np
 from tqdm import tqdm
 from preprocessing.utils import math_module
 from scipy import signal
-import multiprocessing
+import json
+
+import h5py
+
+with open('/home/paperc/PycharmProjects/BPNET/parameter.json') as f:
+    json_data = json.load(f)
+    param = json_data.get("parameters")
+    orders = json_data.get("parameters").get("in_channels")
+    sampling_rate = json_data.get("parameters").get("sampling_rate")
 
 '''
 find_available_data(root_path)
@@ -21,9 +29,9 @@ def find_available_data(root_path):
     print('\n***MIMICdataset.find_available_data() called***')
     person_list = []
     for (path, dirs, files) in os.walk(root_path):
-        for dir in dirs:
-            if len(dir) == 3:
-                person_list.append(dir)
+        for d in dirs:
+            if len(d) == 3:
+                person_list.append(d)
 
     available_list = []
     for person in person_list:
@@ -78,11 +86,12 @@ as attributes in a Record or MultiRecord object.
 ''' 바로 return.p_signal 하도록 변경 '''
 
 
+# TODO -> Record.inti_value를 추가로 return 해주는데, 뒤에 함수들 전부 수정
 def read_record(path, sampfrom=0, sampto=None):
     channel, flag = find_idx(path)
     if flag == 2:
         record = wfdb.rdrecord(path, channels=channel, sampfrom=sampfrom, sampto=sampto)
-        # wfdb.plot_wfdb(record=record, title='Record 039 from PhysioNet MIMIC Dataset')
+        # wfdb.plot_wfdb(record=record, title='Record from PhysioNet MIMIC Dataset')
         # display(record.__dict__)
         return record.p_signal
     else:
@@ -105,25 +114,16 @@ todo
 '''
 
 
-def nan_checker(signal):
-    if (np.isnan(signal.any()) == False) and (np.sum(signal[0][1]) != 0):
-        # print(signal)
-        return signal
-    else:
-        print('nan list found with nan_checker')
-        print(signal)
-        print(signal[0][0], signal[0][1])
-        return None
-
-
-
 def signal_slicing(signals, chunk_num):
     print('\n***MIMICdataset.signal_slicing() called***')
     nan_check_list = []
     refined_sig = []
     dropped_nan = []  # dropped signal chunk index list ( nan included )
     dropped_zero = []  # dropped signal chunk index list ( 0.0 included )
+    print(np.shape(signals))
     r, c = np.shape(signals)
+    print('r :', r)
+    print('c :', c)
     print('before signal_slicing >> np.shape(signals) :', np.shape(signals), '== (', int(r / 75000), '* 75000, 2 )')
 
     # np.shape(signal_nan) : (used_file_cnt(85) * 10, 7500, 2)
@@ -134,21 +134,23 @@ def signal_slicing(signals, chunk_num):
 
     for idx, nan in enumerate(nan_check_list):
         if not nan:
-            if signal_nan[idx][0][1] != 0.0:  # 850개 중 signal_nan의 첫번째줄(abp, ple) 값 중에 ple가 0인 값 제거
-                refined_sig.append(signal_nan[idx])
+            # TODO SIGNAL_NAN에서 이상치를 찾아내는 방식을 변경해야 함 (정규분포 등등 이용)
+            if (signal_nan[idx][0][1] != 0.0) and (signal_nan[idx][0][0] != 80.0) and (signal_nan[idx][0][
+                                                                                           0] < 160):  # 850개 중 signal_nan의 첫번째줄(abp, ple) 값 중에 ple가 0인 값 제거 & abp == 80.0인 값 제거
+                # down sampling
+                refined_sig.append(signal.resample(signal_nan[idx], int(param["chunk_size"] / 125) * sampling_rate["60"]))
             else:
                 dropped_zero.append(idx)
         else:
             dropped_nan.append(idx)
 
     dropped = sorted(dropped_zero + dropped_nan)
-
+    print('before channel splitting :', np.shape(refined_sig))
     refined_sig = np.split(np.array(refined_sig), 2, axis=2)
     abp, ple = np.squeeze(refined_sig[0]), np.squeeze(refined_sig[1])
     print(' >> dropped_nan num :', len(dropped_nan))
     print(' >> dropped_zero num:', len(dropped_zero))
     print(' >> dropped num:', len(dropped))
-    # print(' >> dropped :', dropped)
 
     print('after signal_slicing >> np.shape(ple) :', np.shape(ple), 'np.shape(abp) :', np.shape(abp))
 
@@ -156,67 +158,123 @@ def signal_slicing(signals, chunk_num):
     return ple, abp
 
 
-def data_aggregator(root_path, degree=0, slicefrom=0, sliceto=None):
+def data_aggregator(root_path, degree=0, train=True, percent=0.7):
     print('\n***MIMICdataset.data_aggregator called*** >> degree :', degree)
     available_list = find_available_data(root_path)
+    total_len = len(available_list)
+    train_ = int(total_len*0.7)
+    train_len = int(train_ * percent)
     print('Number of total ICU patient :', len(available_list))
     print(' >>', available_list)
     total_data = np.empty((1, 2))
-    used_list = available_list[slicefrom:sliceto]
+    if train:
+        used_list = available_list[:train_len]
+    else:
+        used_list = available_list[train:]
     total_cnt = 0
     print('Number of selected ICU patient :', len(used_list))
     print(' >>', used_list)
-
-    down_sample = 1800
-
+    used_file_cnt = 0
     for u in used_list:
         p = root_path + u
-        used_file_cnt = 0
         for (path, dirs, files) in os.walk(p):
             for file in tqdm(files):
-                if len(file.split('/')[-1]) == 12 and file.split('.')[-1] == 'hea':
+                if (len(file.split('/')[-1]) == 12) and (file.split('.')[-1] == 'hea'):
                     data_path = (p + '/' + file).strip('.hea')
                     temp_data = read_record(data_path)
                     total_cnt += 1
                     if np.shape(temp_data) == (75000, 2):
                         used_file_cnt += 1
-                        ## down_Sampling
-                        temp_data = signal.resample(temp_data, down_sample * 10)
                         total_data = np.append(total_data, temp_data, axis=0)
                     else:
                         print('\ntrain file dropped :', used_file_cnt, 'th ->> due to file shape is not right :',
                               np.shape(temp_data))
 
     data = total_data[1:]
-    print('np.shape(total_data) :', np.shape(data))
-    print('total_cnt :', total_cnt, '/ used_file_cnt :', used_file_cnt, ' ( dropped_file_cnt :',
+    print('np.shape(data) :', np.shape(data))
+    print('used_file_cnt :', used_file_cnt, ' / total_cnt :', total_cnt, ' ( dropped_file_cnt :',
           total_cnt - used_file_cnt, ')')
-    chunk_num = int(np.shape(data)[0] / down_sample)
-    print('np.shape(data_aggregator input) :', np.shape(data), '== (', int(len(data) / down_sample*10), '* 75000, 2 )')
+
+    chunk_num = int(np.shape(data)[0] / param["chunk_size"])  # / param["down_sample"])
+    print('total_data_lenth :', np.shape(data)[0], ', one chunk length', param["chunk_size"])
+    print('chunk_num :', chunk_num)
+    print('np.shape(signal_slicing input(data) ) :', np.shape(data), '== (', int(np.shape(data)[0] / 75000),
+          '* 75000, 2 )')
 
     if degree == 0:
         ple, abp = signal_slicing(data, chunk_num)  # f
 
         print('*** f data aggregation done***')
-        return ple, abp
+        return ple, abp, train_len
     elif degree == 1:
         ple, abp = signal_slicing(data, chunk_num)  # f
-        ple_first, abp_first = math_module.diff_np(ple, abp)  # f'
+        ple_first = math_module.diff_np(ple)  # f'
+
+        ple_total = ple_first
+        abp_total = abp
+
+        print('*** f\' data aggregation done***')
+        return ple_total, abp_total, train_len
+    elif degree == 2:
+        ple, abp = signal_slicing(data, chunk_num)  # f
+        ple_first = math_module.diff_np(ple)  # f'
+        ple_second = math_module.diff_np(ple_first)  # f''
+
+        ple_total = ple_second
+        abp_total = abp
+        print('*** f\'\' data aggregation done***')
+
+        return ple_total, abp_total, train_len
+    elif degree == 3:
+        ple, abp = signal_slicing(data, chunk_num)  # f
+        ple_first = math_module.diff_np(ple)  # f'
 
         ple_total = math_module.diff_channels_aggregator(ple, ple_first)
         abp_total = abp
 
         print('*** f & f\' data aggregation done***')
-        return ple_total, abp_total
-    elif degree == 2:
+        return ple_total, abp_total, train_len
+    elif degree == 4:
         ple, abp = signal_slicing(data, chunk_num)  # f
-        ple_first, abp_first = math_module.diff_np(ple, abp)  # f'
-        ple_second, abp_second = math_module.diff_np(ple_first, abp_first)  # f''
+        ple_first = math_module.diff_np(ple)  # f'
+        ple_second = math_module.diff_np(ple_first)  # f''
+
+        ple_total = math_module.diff_channels_aggregator(ple, ple_second)
+        abp_total = abp
+        print('*** f & f\'\' data aggregation done***')
+
+        return ple_total, abp_total, train_len
+    elif degree == 5:
+        ple, abp = signal_slicing(data, chunk_num)  # f
+        ple_first = math_module.diff_np(ple)  # f'
+        ple_second = math_module.diff_np(ple_first)  # f''
+
+        ple_total = math_module.diff_channels_aggregator(ple_first, ple_second)
+        abp_total = abp
+        print('*** f\' & f\'\' data aggregation done***')
+
+        return ple_total, abp_total, train_len
+    elif degree == 6:
+        ple, abp = signal_slicing(data, chunk_num)  # f
+        ple_first = math_module.diff_np(ple)  # f'
+        ple_second = math_module.diff_np(ple_first)  # f''
 
         ple_total = math_module.diff_channels_aggregator(ple, ple_first, ple_second)
         abp_total = abp
         print('*** f & f\' & f\'\' data aggregation done***')
 
-        return ple_total, abp_total
+        return ple_total, abp_total, train_len
     else:
         print('derivative not supported... goto data_aggregator()')
+
+
+if __name__ == '__main__':
+    root_path = param["root_path"]
+    order = orders["third"]
+    train_ple, train_abp, data_len = data_aggregator(root_path=root_path, degree=order[1], train=True, percent=0.075)  # 0.05 > 2 patients
+    dset = h5py.File("/home/paperc/PycharmProjects/BPNET/dataset/mimic_BPNet/" + "case(" + str(order[-1])+")_len("+str(data_len)+").hdf5", "w")
+    if len(train_ple) == len(train_abp):
+        dset['ple'] = train_ple
+        dset['abp'] = train_abp
+    dset.close()
+
