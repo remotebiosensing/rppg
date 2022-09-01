37328468fa895fb50e78af6d3a783191d413e60d
paperchae <forownsake@gmail.com>
2022-09-01 22:01:45 +0900

    TODO List / added manual seed for model comparison / added loss4 (L1Loss)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from preprocessing import MIMICdataset, customdataset
+from preprocessing.utils import signal_utils
 from nets.modules.sub_modules.bvp2abp import *
 from nets.loss import loss
 
@@ -13,6 +14,7 @@ import wandb
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+import h5py
 
 with open('parameter.json') as f:
     json_data = json.load(f)
@@ -20,12 +22,6 @@ with open('parameter.json') as f:
     orders = json_data.get("parameters").get("in_channels")
     hyper_param = json_data.get("hyper_parameters")
     wb = json_data.get("wandb")
-
-'''
-wandb setup
-'''
-wandb.init(project="VBPNet", entity="paperchae")
-wandb.config = wb["config"]
 
 print(sys.version)
 #
@@ -36,42 +32,39 @@ print('----- GPU INFO -----\nDevice:', device)  # 출력결과: cuda
 print('Count of using GPUs:', torch.cuda.device_count())
 print('Current cuda device:', torch.cuda.current_device(), '\n--------------------')
 
+torch.manual_seed(125)
+
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(125)
 
 ''' train data load '''
 root_path = param["root_path"]
-slice_range = [0, 2]
-order = orders["second"]
+# TODO TRAIN_PATIENT_RANGE 뿐만 아니라 CUSTOMDATASET에서 TRAIN_DATASET 비율 조정가능하게 변경 Done
+train_patient_range = [0, 2]
+# TODO ORDER랑 TESTORDER 하나로 정리하기 Done
+order = orders["third"]
 '''
     order selects number of model's input channel
     orders["zero"] : use 1 channel ( f )
-    orders["first"] : use 2 channel ( f + f' )
-    orders["second"] : use 3 channel ( f + f' + f'' )
+    orders["first"] : use 1 channel ( f' )
+    orders["second"] : use 1 channel ( f'' )
+    orders["third"] : use 2 channel ( f + f' )
+    orders["fourth"] : use 2 channel ( f + f'' )
+    orders["fifth"] : use 2 channel ( f' + f'' )
+    orders["sixth"] : use 3 channel ( f + f' + f'' )
 '''
-if order == 1:  # return f
-    train_ple, train_abp = MIMICdataset.data_aggregator(
-        root_path=root_path, degree=order - 1, slicefrom=slice_range[0], sliceto=slice_range[1])
-    train_dataset = customdataset.CustomDataset(x_data=train_ple, y_data=train_abp)
-    train_loader = DataLoader(train_dataset, batch_size=hyper_param["batch_size"], shuffle=False)
+preprocessed_data_path = param["preprocessed_data_path"]
 
-elif order == 2:  # return np.concatenate((f, f'), axis=1)
-    train_ple, train_abp = MIMICdataset.data_aggregator(
-        root_path=root_path, degree=order - 1, slicefrom=slice_range[0], sliceto=slice_range[1])
-    train_dataset = customdataset.CustomDataset(x_data=train_ple, y_data=train_abp, degree=order - 1)
-    train_loader = DataLoader(train_dataset, batch_size=hyper_param["batch_size"], shuffle=False)
+train_ple, train_abp, _ = MIMICdataset.data_aggregator(root_path=root_path, degree=order[1],
+                                                       train=True, percent=0.05)  # 0.05 > 2 patients
+train_dataset = customdataset.CustomDataset(x_data=train_ple, y_data=train_abp)
+train_loader = DataLoader(train_dataset, batch_size=hyper_param["batch_size"], shuffle=False)
 
-
-else:  # return np.concatenate((f, f', f''), axis=1)
-    train_ple, train_abp = MIMICdataset.data_aggregator(
-        root_path=root_path, degree=order - 1, slicefrom=slice_range[0], sliceto=slice_range[1])
-    train_dataset = customdataset.CustomDataset(x_data=train_ple, y_data=train_abp, degree=order - 1)
-    train_loader = DataLoader(train_dataset, batch_size=hyper_param["batch_size"], shuffle=False)
-#
-# else:
-#     print('not supported order : please check parameter.json')
-# '''--------------------------'''
-
-''' model train '''
-model = bvp2abp(in_channels=orders["second"], out_channels=16, kernel_size=3).to(device)
+'''   model train     '''
+''' wandb setup '''
+wandb.init(project="VBPNet", entity="paperchae")
+wandb.config = wb["config"]
+model = bvp2abp(in_channels=order[0], out_channels=64, kernel_size=hyper_param["kernel_size"]).to(device)
 
 learning_rate = hyper_param["learning_rate"]
 weight_decay = hyper_param["weight_decay"]
@@ -79,45 +72,40 @@ training_epochs = hyper_param["epochs"]
 
 loss1 = loss.NegPearsonLoss().to(device)
 loss2 = loss.rmseLoss().to(device)
+loss4 = nn.L1Loss(reduction='mean').to(device)
 # loss3 = nn.MSELoss().to(device)
 # loss2 = loss.fftLoss().to(device)
 
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-total_batch = len(train_loader)
+total_batch = train_loader.__len__()
 print('batchN :', total_batch)
 
 costarr = []
-a = 2
-b = 4
-c = 4
-su = a+b+c
-ap = a/su
-bp = b/su
-cp = c/su
+cnt = 0
 for epoch in tqdm(range(training_epochs)):
     avg_cost = 0
-    for idx, samples in enumerate(train_loader):
-        X_train, Y_train = samples
-
+    for idx, sample in enumerate(train_loader):
+        X_train, Y_train = sample
         hypothesis = model(X_train)
         # print('np.shape(hypothesis) :',np.shape(hypothesis))
         optimizer.zero_grad()
 
         '''Negative Pearson Loss'''
-        cost1 = loss1(hypothesis, Y_train)*ap
+        cost1 = loss1(hypothesis, Y_train)
         '''RMSE Loss'''
-        cost2 = loss2(hypothesis, Y_train)*bp
+        # cost2 = loss2(hypothesis, Y_train)
         '''MSE Loss'''
-        cost3 = loss2(hypothesis, Y_train)*cp
+        # cost3 = loss2(hypothesis, Y_train)
+        '''L1 Loss'''
+        cost4 = loss4(hypothesis, Y_train)
 
         '''Total Loss'''
-        if cost3 >= cost2:
-            total_cost0 = cost1 * cost2
-        else:
-            total_cost0 = cost1 * cost3
-
-        cost = total_cost0
+        # if cost3 >= cost2:
+        #     total_cost0 = cost1 * cost2
+        # else:
+        #     total_cost0 = cost1 * cost3
+        cost = cost1 * cost4
         cost.backward()
         optimizer.step()
 
@@ -125,11 +113,14 @@ for epoch in tqdm(range(training_epochs)):
         wandb.log(
             {"Loss": cost,
              "Negative Pearson Loss": cost1,
-             "RMSE Loss": cost2,
-             "MSE Loss:": cost3}, step=epoch)
+             "MAE Loss": cost4}, step=epoch)
+        # "RMSE Loss": cost2,
+        # "MSE Loss:": cost3}, step=epoch)
 
     costarr.append(avg_cost.__float__())
-    print('     ->     avg_cost == ', avg_cost.__float__())
+    # if avg_cost == np.nan:
+    #     print('nan idx : ', idx)
+    # print('     ->     avg_cost == ', type(avg_cost.__float__()), avg_cost.__float__())
 print('cost :', costarr[-1])
 
 t_val = np.array(range(len(costarr)))
@@ -139,6 +130,12 @@ plt.show()
 
 # model save
 PATH = param["save_path"]
-torch.save(model, PATH + 'model_derivative_test.pt')
+# torch.save(model, PATH + 'model_' + str(order - 1) + '_NegMAE_baseline.pt')
+torch.save(model, PATH + 'model_110_NegMAE_newmodel_temp.pt')
 # torch.save(model.state_dict(), PATH + 'model_state_dict.pt')
 torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict()}, PATH + 'all.tar')
+
+# TODO 1. 혈압기기와 기준기기의 차이의 정도에 따라 모델의 등급이 나뉘는 것 찾아보기
+# TODO 2. PPNET 참고하여 평가지표 확인하기
+# TODO 3. SAMPLING RATE 에 따른 차이 확인
+# TODO 4. READRECORD() 순서는 [ABP,PLE] / 나머지 순서 다 맞추기
