7ca110f5e3c3dadaba147f4239ec4ecf067b4b76
paperchae <forownsake@gmail.com>
2022-09-20 01:27:44 +0900

    changed to return [l_out(signal's shape), sbp, amp] by including amplitude_module

--- nets/modules/bvp2abp.py
+++ nets/modules/bvp2abp.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 from nets.modules.sub_modules.Trend_module import Trend_module_1D
-from nets.modules.sub_modules.Amplitude_module import Amplitude_module_1D
+from nets.modules.sub_modules.Detail_module import Detail_module_1D
 from nets.modules.sub_modules.Linear_module import Linear_module_1D
+from nets.modules.sub_modules.Amplitude_module import Amplitude_module
 
 
 class bvp2abp(nn.Module):
@@ -11,17 +12,17 @@ class bvp2abp(nn.Module):
         self.in_channel = in_channels
 
         self.trend_model = Trend_module_1D(self.in_channel)
-        self.amplitude_model = Amplitude_module_1D(self.in_channel)
+        self.detail_model = Detail_module_1D(self.in_channel)
         self.linear_model = Linear_module_1D()
+        self.amplitude_model = Amplitude_module()
 
-        self.conv1 = torch.nn.Sequential(
 
-        )
 
     def forward(self, ple_input):
         ple_input = torch.reshape(ple_input, (-1, self.in_channel, 360))  # [ batch , channel, size]
-        at1, at2 = self.amplitude_model.forward(ple_input)
+        at1, at2 = self.detail_model.forward(ple_input)
         t_out = self.trend_model.forward(ple_input, at1, at2)
         l_out = self.linear_model.forward(t_out)
+        sbp, amp = self.amplitude_model.forward(l_out)
 
-        return l_out
+        return l_out, sbp, amp
