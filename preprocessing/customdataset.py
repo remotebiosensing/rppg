6ae79eb6bbb0a3b8db95a1a699aadd4f12f40924
paperchae <forownsake@gmail.com>
2022-09-20 01:31:20 +0900

    dataset_selector.py and customdataset.py had been modified according to change of signal_slicing()

--- preprocessing/customdataset.py
+++ preprocessing/customdataset.py
@@ -1,25 +1,29 @@
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 import numpy as np
 
 
 class CustomDataset(Dataset):
-    def __init__(self, x_data, y_data):
-        # 들어온 x는 tensor형태로 변환
+    def __init__(self, x_data, y_data, size_factor):
         self.x_data = torch.FloatTensor(x_data)
-        # tensor data의 형태는 (배치사이즈, 채널사이즈, 이미지 너비, 높이)의 형태임
-        # 따라서 들어온 데이터의 형식을 permute함수를 활용하여 바꾸어주어야함.
-        # self.x_data = self.x_data.permute(0, 3, 1, 2)  # 인덱스 번호로 바꾸어주는 것 # 이미지 개수, 채널 수, 이미지 너비, 높이
-        print('shape of x_data : ', np.shape(self.x_data))
-        self.y_data = torch.FloatTensor(y_data)  # float tensor / long tensor 로 숫자 속성을 정해줄 수 있음
-        self.len = self.y_data.shape[0]
 
-    # x,y를 튜플형태로 내보내기
+        print('shape of x_data : ', np.shape(self.x_data))
+        self.y_data = torch.FloatTensor(y_data)
+        print('shape of y_data : ', np.shape(self.y_data))
+        self.len = self.y_data.shape[0]
+        self.size = torch.FloatTensor(size_factor)
+        print('shape of size_data : ', np.shape(self.size))
+
     def __getitem__(self, index):
         # self.x_data = torch.reshape(self.x_data, [1, 1, len(self.x_data)])
         x = self.x_data[index].to('cuda')
         y = self.y_data[index].to('cuda')
-        return x, y
+        # d = torch.squeeze(self.size[index][0]).to('cuda')
+        s = self.size[index][1].to('cuda')
+        a = (self.size[index][1]-self.size[index][0]).to('cuda')
+        '''variable a is dependent variable of s and d'''
+        # m = self.size[index][2].to('cuda')
+        return x, y, s, a #d, s, m
 
     def __len__(self):
         return self.len
--- preprocessing/dataset_selector.py
+++ preprocessing/dataset_selector.py
@@ -9,7 +9,8 @@ with open('/home/paperc/PycharmProjects/VBPNet/config/parameter.json') as f:
     orders = json_data.get("parameters").get("in_channels")
     sampling_rate = json_data.get("parameters").get("sampling_rate")
 
-order = orders["third"]
+order = orders["sixth"]
+samp_rate = sampling_rate["60"]
 
 
 def selector(dataset="mimic", degree=order[1]):
@@ -17,22 +18,28 @@ def selector(dataset="mimic", degree=order[1]):
     write_path = root_path + data_path[dataset][1]
     if dataset == "mimic":
         import MIMICdataset
-        train_abp, train_ple, data_len = MIMICdataset.data_aggregator(root_path=read_path, degree=degree,
-                                                                      train=True, percent=0.05)  # 0.05 -> 2 patients
         print("mimic dataset selected")
+        train_abp, train_ple, data_len = MIMICdataset.data_aggregator(root_path=read_path, degree=degree,
+                                                                      train=True, percent=0.05,
+                                                                      samp_rate=samp_rate)  # 0.05 -> 2 patients
     else:  # uci
         import UCIdataset
-        train_abp, train_ple, data_len = UCIdataset.data_aggregator(root_path=read_path, degree=degree,
-                                                                    train=True, percent=0.25)  # 0.25 -> 3000 patients (total 12000 patient)
         print("uci dataset selected")
+        train_abp, train_ple, train_size, data_len = UCIdataset.data_aggregator(root_path=read_path, degree=degree,
+                                                                            train=True, percent=0.75,
+                                                                            samp_rate=samp_rate)  # 0.25 -> 3000 patients (total 12000 patient)
 
     dset = h5py.File(write_path + "case(" + str(order[-1]) + ")_len(" + str(data_len) +
-                     ")_" + str(int(param["chunk_size"] / 125) * sampling_rate["60"]) + ".hdf5", "w")
+                     ")_" + str(int(param["chunk_size"] / sampling_rate["base"]) * samp_rate) + "_09_size_factor2.hdf5",
+                     "w")
     if len(train_ple) == len(train_abp):
         dset['ple'] = train_ple
         dset['abp'] = train_abp
+        dset['size'] = train_size
+        # dset['size'] = size_factor
+    else:
+        print("length of ple and abp doesn't match")
     dset.close()
 
 
 selector("uci")
-
