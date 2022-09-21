085d1157ec2108ddd4f8e95bc03285d320b2b466
paperchae <forownsake@gmail.com>
2022-09-21 22:38:41 +0900

    changed to get sbp,amp -> dbp, sbp

--- nets/modules/sub_modules/Amplitude_module.py
+++ nets/modules/sub_modules/Amplitude_module.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 
@@ -6,18 +5,22 @@ class Amplitude_module(nn.Module):
     def __init__(self):
         super(Amplitude_module, self).__init__()
 
-        # self.dbp = nn.AdaptiveAvgPool1d(1)  # dbp
+        # self.dbp = nn.Sequential(
+        #
+        # )
+        self.dbp = nn.AdaptiveAvgPool1d(1)  # dbp
         self.sbp = nn.AdaptiveAvgPool1d(1)  # sbp
         self.amp = nn.AdaptiveAvgPool1d(1)  # amplitude of abp
         # self.mbp = nn.Linear(360, 1)  # mbp
 
 
-    def forward(self, linear_out):
-        sbp = self.sbp(linear_out)
+    def forward(self, feature):
+        dbp = self.dbp(feature)
+        sbp = self.sbp(feature)
         '''
         to reduce load of model 
         amp is set dependent variable of sbp
         '''
-        amp = self.amp(sbp)
+        # amp = self.amp(sbp)
 
-        return sbp, amp
+        return dbp, sbp
