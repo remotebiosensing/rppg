7975a88664186e1cd0d35f51a8d0e614527e3d16
paperchae <forownsake@gmail.com>
2022-09-21 22:52:07 +0900

    train boolean flag added to divide train, test dataset

--- preprocessing/dataset_selector.py
+++ preprocessing/dataset_selector.py
@@ -13,25 +13,25 @@ order = orders["sixth"]
 samp_rate = sampling_rate["60"]
 
 
-def selector(dataset="mimic", degree=order[1]):
+def selector(dataset="mimic", degree=order[1], train=True):
     read_path = root_path + data_path[dataset][0]
     write_path = root_path + data_path[dataset][1]
     if dataset == "mimic":
         import MIMICdataset
         print("mimic dataset selected")
         train_abp, train_ple, data_len = MIMICdataset.data_aggregator(root_path=read_path, degree=degree,
-                                                                      train=True, percent=0.05,
+                                                                      train=train, percent=0.05,
                                                                       samp_rate=samp_rate)  # 0.05 -> 2 patients
     else:  # uci
         import UCIdataset
         print("uci dataset selected")
         train_abp, train_ple, train_size, data_len = UCIdataset.data_aggregator(root_path=read_path, degree=degree,
-                                                                            train=True, percent=0.75,
-                                                                            samp_rate=samp_rate)  # 0.25 -> 3000 patients (total 12000 patient)
+                                                                                train=train, percent=0.75,
+                                                                                samp_rate=samp_rate)  # 0.25 -> 3000 patients (total 12000 patient)
 
-    dset = h5py.File(write_path + "case(" + str(order[-1]) + ")_len(" + str(data_len) +
-                     ")_" + str(int(param["chunk_size"] / sampling_rate["base"]) * samp_rate) + "_09_size_factor2.hdf5",
-                     "w")
+    dset = h5py.File(write_path + "case(" + str(order[-1]) + ")_len(" + str(data_len) + ")_"
+                     + str(int(param["chunk_size"] / sampling_rate["base"]) * samp_rate)
+                     + "_train(" + str(train)+").hdf5", "w")
     if len(train_ple) == len(train_abp):
         dset['ple'] = train_ple
         dset['abp'] = train_abp
@@ -42,4 +42,4 @@ def selector(dataset="mimic", degree=order[1]):
     dset.close()
 
 
-selector("uci")
+selector("uci", train=False)
