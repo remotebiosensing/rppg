2f713618df2e75672fdcecd520f93bc94873744b
paperchae <forownsake@gmail.com>
2022-09-23 19:38:18 +0900

    changed nn.adaptivepool -> nn.linear

--- nets/modules/sub_modules/Amplitude_module.py
+++ nets/modules/sub_modules/Amplitude_module.py
@@ -5,14 +5,23 @@ class Amplitude_module(nn.Module):
     def __init__(self):
         super(Amplitude_module, self).__init__()
 
-        # self.dbp = nn.Sequential(
-        #
-        # )
-        self.dbp = nn.AdaptiveAvgPool1d(1)  # dbp
-        self.sbp = nn.AdaptiveAvgPool1d(1)  # sbp
-        self.amp = nn.AdaptiveAvgPool1d(1)  # amplitude of abp
-        # self.mbp = nn.Linear(360, 1)  # mbp
+        self.dbp = nn.Sequential(
+            nn.Linear(100, 50),
+            nn.Dropout1d(0.5),
+            nn.Linear(50, 1),
+            nn.LeakyReLU()
 
+        )
+        self.sbp = nn.Sequential(
+            nn.Linear(100, 50),
+            nn.Dropout1d(0.5),
+            nn.Linear(50, 1),
+            nn.LeakyReLU()
+        )
+        # self.dbp = nn.AdaptiveAvgPool1d(1)  # dbp
+        # self.sbp = nn.AdaptiveAvgPool1d(1)  # sbp
+        # self.amp = nn.AdaptiveAvgPool1d(1)  # amplitude of abp
+        # self.mbp = nn.Linear(360, 1)  # mbp
 
     def forward(self, feature):
         dbp = self.dbp(feature)
