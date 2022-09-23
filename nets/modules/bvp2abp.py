43692132e1bb097b3a643928f683b5d623808a5b
paperchae <forownsake@gmail.com>
2022-09-23 19:39:45 +0900

    changed the order of trend_module and detail_module

--- nets/modules/bvp2abp.py
+++ nets/modules/bvp2abp.py
@@ -20,9 +20,13 @@ class bvp2abp(nn.Module):
 
     def forward(self, ple_input):
         ple_input = torch.reshape(ple_input, (-1, self.in_channel, 360))  # [ batch , channel, size]
-        at1, at2 = self.detail_model.forward(ple_input)
-        t_out = self.trend_model.forward(ple_input, at1, at2)
-        l_out = self.linear_model.forward(t_out)
-        dbp, sbp = self.amplitude_model.forward(t_out)
+        # at1, at2 = self.detail_model.forward(ple_input)
+        # t_out = self.trend_model.forward(ple_input, at1, at2)
+        # l_out = self.linear_model.forward(t_out)
+        # dbp, sbp = self.amplitude_model.forward(t_out)
+        at1, at2 = self.trend_model.forward(ple_input)
+        d_out = self.detail_model.forward(ple_input,at1,at2)
+        l_out = self.linear_model.forward(d_out)
+        dbp, sbp = self.amplitude_model.forward(d_out)
 
         return l_out, dbp, sbp
