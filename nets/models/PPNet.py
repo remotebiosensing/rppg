c57a6fe5af4553a060af9139eb0e8f016875f8b7
dae yeol kim <kwelcomm@gmail.com>
2022-09-26 11:23:28 +0900

    * refactoring - PPNet.py

--- nets/models/PPNet.py
+++ nets/models/PPNet.py
@@ -1,26 +1,22 @@
 import torch
 
-from nets.models.sub_models.AppearanceModel import AppearanceModel_2D
-from nets.models.sub_models.LinearModel import LinearModel
-from nets.models.sub_models.MotionModel import MotionModel
-
 
 class PPNet(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1d_1 = torch.nn.Conv1d(1,20,9)
+        self.conv1d_1 = torch.nn.Conv1d(1, 20, 9)
         self.maxpool1d_1 = torch.nn.MaxPool1d(4)
         self.drop_1 = torch.nn.Dropout(0.1)
-        self.conv1d_2 = torch.nn.Conv1d(20,20,9)
+        self.conv1d_2 = torch.nn.Conv1d(20, 20, 9)
         self.maxpool1d_2 = torch.nn.MaxPool1d(4)
         self.drop_2 = torch.nn.Dropout(0.1)
-        self.lstm_1 = torch.nn.LSTM(13,64)
+        self.lstm_1 = torch.nn.LSTM(13, 64)
         self.drop_3 = torch.nn.Dropout(0.1)
-        self.lstm_2 = torch.nn.LSTM(64,128)
+        self.lstm_2 = torch.nn.LSTM(64, 128)
         self.drop_4 = torch.nn.Dropout(0.1)
         self.flatten = torch.nn.Flatten()
-        self.dense = torch.nn.Linear(2560,3)
+        self.dense = torch.nn.Linear(2560, 3)
 
         self.relu = torch.nn.ReLU()
         self.tanh = torch.nn.Tanh()
