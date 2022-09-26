5152c0534ed901e02f166fe00793988d711b3f3c
dae yeol kim <kwelcomm@gmail.com>
2022-09-26 15:26:46 +0900

    * refactoring - fix main.py, dataset_loader.py, dataset_preprocess.py

--- dataset/dataset_loader.py
+++ dataset/dataset_loader.py
@@ -1,15 +1,34 @@
+import os
+
 import h5py
 import numpy as np
+from torch.utils.data import DataLoader
 
+from dataset.AxisNetDataset import AxisNetDataset
 from dataset.DeepPhysDataset import DeepPhysDataset
+from dataset.GCNDataset import GCNDataset
 from dataset.PPNetDataset import PPNetDataset
 from dataset.PhysNetDataset import PhysNetDataset
 
 
-def dataset_loader(save_root_path: str = "/media/hdd1/dy_dataset/",
+def split_data_loader(datasets, batch_size, train_shuffle, test_shuffle=False):
+    if datasets.__len__() == 3:
+        train_loader = DataLoader(datasets[0], batch_size=batch_size, shuffle=train_shuffle)
+        validation_loader = DataLoader(datasets[1], batch_size=batch_size, shuffle=test_shuffle)
+        test_loader = DataLoader(datasets[2], batch_size=batch_size, shuffle=test_shuffle)
+        return [train_loader, validation_loader, test_loader]
+    elif datasets.__len__() == 2:
+        train_loader = DataLoader(datasets[0], batch_size=batch_size, shuffle=train_shuffle)
+        test_loader = DataLoader(datasets[1], batch_size=batch_size, shuffle=test_shuffle)
+        return [train_loader, test_loader]
+
+
+def dataset_loader(save_root_path: str = "/media/hdd1/dy/dataset/",
                    model_name: str = "DeepPhys",
                    dataset_name: str = "UBFC",
-                   option: str = "train"):
+                   option: str = "train",
+                   log_flag: bool = True
+                   ):
     '''
     :param save_root_path: save file destination path
     :param model_name : model_name
@@ -17,7 +36,18 @@ def dataset_loader(save_root_path: str = "/media/hdd1/dy_dataset/",
     :param option:[train, test]
     :return: dataset
     '''
-    hpy_file = h5py.File(save_root_path + model_name + "_" + dataset_name + "_" + option + ".hdf5", "r")
+    if log_flag:
+        print("========= dataset_loader() in" + os.path.basename(__file__))
+
+    cnt = 0
+    flag = True
+    name = model_name
+    if model_name == "GCN" or model_name == "GCN_TEST":
+        name = "PhysNet"
+    hpy_train_file = h5py.File(save_root_path + name + "_" + dataset_name + "_" + "train" + ".hdf5", "r")
+    hpy_test_file = h5py.File(save_root_path + name + "_" + dataset_name + "_" + "test" + ".hdf5", "r")
+
+    graph_file = save_root_path + model_name + "_" + dataset_name + "_" + option + ".pkl"
 
     if model_name in ["DeepPhys", "MTTS"]:
         appearance_data = []
@@ -28,21 +58,61 @@ def dataset_loader(save_root_path: str = "/media/hdd1/dy_dataset/",
             appearance_data.extend(hpy_file[key]['preprocessed_video'][:, :, :, -3:])
             motion_data.extend(hpy_file[key]['preprocessed_video'][:, :, :, :3])
             target_data.extend(hpy_file[key]['preprocessed_label'])
+
         hpy_file.close()
 
         dataset = DeepPhysDataset(appearance_data=np.asarray(appearance_data),
                                   motion_data=np.asarray(motion_data),
                                   target=np.asarray(target_data))
-    elif model_name in ["PhysNet", "PhysNet_LSTM"]:
+    elif model_name in ["PhysNet", "PhysNet_LSTM", "GCN"]:
         video_data = []
         label_data = []
-        for key in hpy_file.keys():
-            video_data.extend(hpy_file[key]['preprocessed_video'])
-            label_data.extend(hpy_file[key]['preprocessed_label'])
-        hpy_file.close()
+        bpm_data = []
+        cnt = 0
+        # for key in hpy_train_file.keys():
+        #     cnt += 1
+        #     if len(hpy_train_file[key]['preprocessed_video']) == len(hpy_train_file[key]['preprocessed_label']):
+        #         video_data.extend(hpy_train_file[key]['preprocessed_video'])
+        #         label_data.extend(hpy_train_file[key]['preprocessed_label'])
+        # bpm_data.extend(hpy_train_file[key]['preprocessed_hr'])
 
-        dataset = PhysNetDataset(video_data=np.asarray(video_data),
-                                 label_data=np.asarray(label_data))
+        # if option == "test" or flag :
+        # if cnt == 4:
+        # break
+        # hpy_train_file.close()
+        for key in hpy_test_file.keys():
+            cnt += 1
+            # if cnt <cflag:
+            #     continue
+            # if cnt < cflag:
+            #     continue
+            # if cnt > cflag:
+            #     break
+            if len(hpy_test_file[key]['preprocessed_video']) == len(hpy_test_file[key]['preprocessed_label']):
+                video_data.extend(hpy_test_file[key]['preprocessed_video'])
+                label_data.extend(hpy_test_file[key]['preprocessed_label'])
+                # bpm_data.extend(hpy_test_file[key]['preprocessed_hr'])
+            if cnt == 4:
+                break
+            # if cnt == cflag:
+            #     break
+
+            # if option == "test" or flag :
+            #     break
+        hpy_test_file.close()
+
+        if model_name in ["GCN"]:
+            dataset = GCNDataset(video_data=np.asarray(video_data),
+                                 label_data=np.asarray(label_data),
+                                 bpm_data=np.asarray(bpm_data)
+                                 )
+        elif model_name in ["AxisNet"]:
+            dataset = AxisNetDataset(video_data=np.asarray(video_data),
+                                     label_data=np.asarray(label_data))
+
+        else:
+            dataset = PhysNetDataset(video_data=np.asarray(video_data),
+                                     label_data=np.asarray(label_data))
 
     elif model_name in ["PPNet"]:
         ppg = []
@@ -76,5 +146,24 @@ def dataset_loader(save_root_path: str = "/media/hdd1/dy_dataset/",
         dataset = PPNetDataset(face_data=np.asarray(face_data),
                                mask_data=np.asarray(mask_data),
                                target=np.asarray(target_data))
+    elif model_name in ["AxisNet"]:
+        video_data = []
+        label_data = []
+        ptt_data = []
+
+        for key in hpy_file.keys():
+            video_data.extend(hpy_file[key]['preprocessed_video'])
+            ptt_data.extend(hpy_file[key]['preprocessed_ptt'])
+            label_data.extend(hpy_file[key]['preprocessed_label'])
+        hpy_file.close()
+
+        std_shape = (320, 472, 3)  # ptt_data[0].shape
+        for i in range(len(ptt_data)):
+            if ptt_data[i].shape != std_shape:
+                ptt_data[i] = np.resize(ptt_data[i], std_shape)
+
+        dataset = AxisNetDataset(video_data=np.asarray(video_data),
+                                 ptt_data=np.asarray(ptt_data),
+                                 label_data=np.asarray(label_data), )
 
     return dataset
--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 import copy
 import datetime
 import json
+import math
 import time
 import os
-
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
-from dataset.dataset_loader import dataset_loader
+from dataset.dataset_loader import dataset_loader, split_data_loader
 from log import log_info_time
 from loss import loss_fn
 from models import is_model_support, get_model, summary
 from optim import optimizer
 from torch.optim import lr_scheduler
-from utils.dataset_preprocess import preprocessing
+
+from utils.TorchLossComputer import TorchLossComputer
+from utils.dataset_preprocess import preprocessing, dataset_split
 from utils.funcs import normalize, plot_graph, detrend
+from utils.funcs import detrend
+from heartpy import process
+from sklearn.model_selection import KFold
+import config as config
+import wandb
+from utils.image_preprocess import get_haarcascade
+from biosppy import ppg
+import pyVHR
+from utils.train import train_fn,test_fn
+
+bpm_flag = False
+K_Fold_flag = False
+model_save_flag = False
+log_flag = True
+wandb_flag = True
+
+# Define Kfold Cross Validator
+if K_Fold_flag:
+    kfold = KFold(n_splits=5, shuffle=True)
+
+if wandb_flag:
+    wandb.init(project="SeqNet", entity="daeyeolkim")
+
+now = datetime.datetime.now()
+os.environ["CUDA_VISIBLE_DEVICES"] = "9"
 
 with open('params.json') as f:
     jsonObject = json.load(f)
@@ -29,9 +57,13 @@ with open('params.json') as f:
     model_params = jsonObject.get("model_params")
 #
 """
+TEST FOR LOAD
+"""
+# model = torch.load(params["model_root_path"] +model_params["name"] +params["dataset_name"] )
+"""
 Check Model Support
 """
-is_model_support(model_params["name"], model_params["name_comment"])
+is_model_support(model_params["name"], model_params["name_comment"], log_flag)
 '''
 Generate preprocessed data hpy file 
 '''
@@ -43,9 +75,28 @@ if __PREPROCESSING__:
                   model_name=model_params["name"],
                   data_root_path=params["data_root_path"],
                   dataset_name=params["dataset_name"],
-                  train_ratio=params["train_ratio"])
+                  train_ratio=params["train_ratio"],
+                  log_flag=log_flag)
     if __TIME__:
         log_info_time("preprocessing time \t:", datetime.timedelta(seconds=time.time() - start_time))
+
+print("Set Model")
+'''
+Setting Learning Model
+'''
+if __TIME__:
+    start_time = time.time()
+
+# model = [get_model(model_params["name"])]
+model = get_model(model_params["name"], log_flag).cuda()
+
+if __MODEL_SUMMARY__:
+    summary(model, model_params["name"], log_flag)
+
+torch.save(model.state_dict(), params["model_root_path"] + model_params["name"] + params["dataset_name"] + "W")
+
+if __TIME__:
+    log_info_time("model initialize time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
 '''
 Load dataset before using Torch DataLoader
@@ -57,22 +108,18 @@ dataset = dataset_loader(save_root_path=params["save_root_path"],
                          model_name=model_params["name"],
                          dataset_name=params["dataset_name"],
                          option="train")
-
-train_dataset, validation_dataset = random_split(dataset,
-                                                 [int(np.floor(
-                                                     len(dataset) * params["validation_ratio"])),
-                                                     int(np.ceil(
-                                                         len(dataset) * (1 - params["validation_ratio"])))]
-                                                 )
+# test_dataset = dataset_loader(save_root_path=params["save_root_path"],
+#                               model_name=model_params["name"],
+#                               dataset_name=params["dataset_name"],
+#                               option="test")
+if not K_Fold_flag:
+    datasets = dataset_split(dataset,[0.7,0.1,0.2])
 if __TIME__:
     log_info_time("load train hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
 if __TIME__:
     start_time = time.time()
-test_dataset = dataset_loader(save_root_path=params["save_root_path"],
-                              model_name=model_params["name"],
-                              dataset_name=params["dataset_name"],
-                              option="test")
+
 if __TIME__:
     log_info_time("load test hpy time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
@@ -81,45 +128,19 @@ if __TIME__:
 '''
 if __TIME__:
     start_time = time.time()
-train_loader = DataLoader(train_dataset, batch_size=params["train_batch_size"],
-                          shuffle=params["train_shuffle"])
-validation_loader = DataLoader(validation_dataset, batch_size=params["train_batch_size"],
-                               shuffle=params["train_shuffle"])
-test_loader = DataLoader(test_dataset, batch_size=params["test_batch_size"],
-                         shuffle=params["test_shuffle"])
+if not K_Fold_flag:
+    data_loaders = split_data_loader(datasets,params["train_batch_size"],params["train_shuffle"],params["test_shuffle"])
+
 if __TIME__:
     log_info_time("generate dataloader time \t: ", datetime.timedelta(seconds=time.time() - start_time))
-'''
-Setting Learning Model
-'''
-if __TIME__:
-    start_time = time.time()
-model = get_model(model_params["name"])
-if torch.cuda.is_available():
-    # os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
-    # TODO: implement parallel training
-    # if options["parallel_criterion"] :
-    #     print(options["parallel_criterion_comment"])
-    #     model = DataParallelModel(model, device_ids=[0, 1, 2])
-    # else:
-    #     model = DataParallel(model, output_device=0)
-    torch.cuda.set_device(int(options["set_gpu_device"]))
-    model.cuda()
-else:
-    model = model.to('cpu')
-
-if __MODEL_SUMMARY__:
-    summary(model,model_params["name"])
-
-if __TIME__:
-    log_info_time("model initialize time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
 '''
 Setting Loss Function
 '''
 if __TIME__:
     start_time = time.time()
-criterion = loss_fn(hyper_params["loss_fn"])
+criterion = loss_fn(hyper_params["loss_fn"], log_flag)
+criterion2 = loss_fn("L1", log_flag)
 
 # if torch.cuda.is_available():
 # TODO: implement parallel training
@@ -134,8 +155,18 @@ Setting Optimizer
 '''
 if __TIME__:
     start_time = time.time()
+# optimizer = [optimizer(mod.parameters(),hyper_params["learning_rate"], hyper_params["optimizer"]) for mod in model[0]]
+# scheduler = [lr_scheduler.ExponentialLR(optim,gamma=0.99) for optim in optimizer]
 optimizer = optimizer(model.parameters(), hyper_params["learning_rate"], hyper_params["optimizer"])
-scheduler = lr_scheduler.ExponentialLR(optimizer,gamma=0.99)
+scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+
+if wandb_flag:
+    wandb.config = {
+        "learning_rate": hyper_params["learning_rate"],
+        "epochs": hyper_params["epochs"],
+        "batch_size": params["train_batch_size"]
+    }
+
 if __TIME__:
     log_info_time("setting optimizer time \t: ", datetime.timedelta(seconds=time.time() - start_time))
 
@@ -145,105 +176,23 @@ Model Training Step
 min_val_loss = 100.0
 min_val_loss_model = None
 
+
+val_loss = 1.0
+test_flag = False
+
+a_start = 0.1
+b_start = 1.0
+exp_a = 0.5
+exp_b = 5.0
+
+#dataloaders
+
+
+
 for epoch in range(hyper_params["epochs"]):
-    if __TIME__ and epoch == 0:
-        start_time = time.time()
-    with tqdm(train_loader, desc="Train ", total=len(train_loader)) as tepoch:
-        model.train()
-        running_loss = 0.0
-        i = 0
-        for inputs, target in tepoch:
-            tepoch.set_description(f"Train Epoch {epoch}")
-            outputs = model(inputs)
+    train_fn(epoch,model,optimizer,criterion,data_loaders[0],"Train",True)
+    if data_loaders.__len__() == 3:
+        test_fn(epoch,model,optimizer,criterion,data_loaders[1],"Val",True,False)
+    if epoch % 10 == 0:
+        running_loss = test_fn(epoch, model, optimizer, criterion, data_loaders[-1], "Test", True, True)
 
-            if model_params["name"] in ["PhysNet", "PhysNet_LSTM","DeepPhys"]:
-                loss = criterion(outputs, target)
-            else:
-                loss_0 = criterion(outputs[:][0], target[:][0])
-                loss_1 = criterion(outputs[:][1], target[:][1])
-                loss_2 = criterion(outputs[:][2], target[:][2])
-                loss = loss_0 + loss_2 + loss_1
-
-            if ~torch.isfinite(loss):
-                continue
-
-            optimizer.zero_grad()
-            loss.backward()
-            running_loss += loss.item()
-            optimizer.step()
-            tepoch.set_postfix(loss=running_loss / params["train_batch_size"])
-    if __TIME__ and epoch == 0:
-        log_info_time("1 epoch training time \t: ", datetime.timedelta(seconds=time.time() - start_time))
-
-    with tqdm(validation_loader, desc="Validation ", total=len(validation_loader)) as tepoch:
-        model.eval()
-        running_loss = 0.0
-        with torch.no_grad():
-            for inputs, target in tepoch:
-                tepoch.set_description(f"Validation")
-                outputs = model(inputs)
-                if model_params["name"] in ["PhysNet", "PhysNet_LSTM", "DeepPhys"]:
-                    loss = criterion(outputs, target)
-                else:
-                    loss_0 = criterion(outputs[:][0], target[:][0])
-                    loss_1 = criterion(outputs[:][1], target[:][1])
-                    loss_2 = criterion(outputs[:][2], target[:][2])
-                    loss = loss_0 + loss_2 + loss_1
-
-                if ~torch.isfinite(loss):
-                    continue
-                running_loss += loss.item()
-                tepoch.set_postfix(loss=running_loss / params["train_batch_size"])
-            if min_val_loss > running_loss:  # save the train model
-                min_val_loss = running_loss
-                checkpoint = {'Epoch': epoch,
-                              'state_dict': model.state_dict(),
-                              'optimizer': optimizer.state_dict()}
-                torch.save(checkpoint, params["checkpoint_path"] + model_params["name"] + "/"
-                           + params["dataset_name"] + "_" + str(epoch) + "_"
-                           + str(min_val_loss) + '.pth')
-                min_val_loss_model = copy.deepcopy(model)
-
-    if epoch + 1 == hyper_params["epochs"] or epoch % 10 == 0:
-        if __TIME__ and epoch == 0:
-            start_time = time.time()
-        if epoch + 1 == hyper_params["epochs"]:
-            model = min_val_loss_model
-        with tqdm(test_loader, desc="test ", total=len(test_loader)) as tepoch:
-            model.eval()
-            inference_array = []
-            target_array = []
-            with torch.no_grad():
-                for inputs, target in tepoch:
-                    tepoch.set_description(f"test")
-                    outputs = model(inputs)
-                    if model_params["name"] in ["PhysNet", "PhysNet_LSTM", "DeepPhys"]:
-                        loss = criterion(outputs, target)
-                    else:
-                        loss_0 = criterion(outputs[:][0], target[:][0])
-                        loss_1 = criterion(outputs[:][1], target[:][1])
-                        loss_2 = criterion(outputs[:][2], target[:][2])
-                        loss = loss_0 + loss_2 + loss_1
-
-                    if ~torch.isfinite(loss):
-                        continue
-                    running_loss += loss.item()
-                    tepoch.set_postfix(loss=running_loss / (params["train_batch_size"] / params["test_batch_size"]))
-                    if model_params["name"] in ["PhysNet","PhysNet_LSTM"]:
-                        inference_array.extend(normalize(outputs.cpu().numpy()[0]))
-                        target_array.extend(normalize(target.cpu().numpy()[0]))
-                    else:
-                        inference_array.extend(outputs[:][0].cpu().numpy())
-                        target_array.extend(target[:][0].cpu().numpy())
-                    if tepoch.n == 0 and __TIME__:
-                        save_time = time.time()
-
-            # postprocessing
-            if model_params["name"] in ["DeepPhys"]:
-                inference_array = detrend(np.cumsum(inference_array),100)
-                target_array = detrend(np.cumsum(target_array),100)
-
-            if __TIME__ and epoch == 0:
-                log_info_time("inference time \t: ", datetime.timedelta(seconds=save_time - start_time))
-
-            plot_graph(0, 300, target_array, inference_array)
