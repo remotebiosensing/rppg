518fba0c2d746e123b9f171b12f539c988b09c4d
dae yeol kim <kwelcomm@gmail.com>
2022-09-26 15:42:21 +0900

    * refactoring - fix error

--- models.py
+++ models.py
@@ -2,18 +2,30 @@ import torchinfo
 import torchsummary
 
 from log import log_warning, log_info
+from nets.models.AxisNet import AxisNet,PhysiologicalGenerator
 from nets.models.DeepPhys import DeepPhys
 from nets.models.DeepPhys_DA import DeepPhys_DA
 from nets.models.PPNet import PPNet
 from nets.models.PhysNet import PhysNet
 from nets.models.PhysNet import PhysNet_2DCNN_LSTM
+from nets.models.Seq_GCN import Seq_GCN
+from nets.models.TEST import TEST,TEST2
+import os
+NUM_FEATURES = 5
+NUM_CLASSES = 10
+
+def get_ver_model(model_name: str = "DeepPhys", ver:int = 1):
+    return TEST2(ver)
 
 
-def get_model(model_name: str = "DeepPhys"):
+def get_model(model_name: str = 'DeepPhys', log_flag:bool = True):
     """
     :param model_name: model name
     :return: model
     """
+    if log_flag:
+        print("========= set model get_model() in"+ os.path.basename(__file__))
+
     if model_name == "DeepPhys":
         return DeepPhys()
     elif model_name == "DeepPhys_DA":
@@ -24,17 +36,23 @@ def get_model(model_name: str = "DeepPhys"):
         return PhysNet_2DCNN_LSTM()
     elif model_name == "PPNet":
         return PPNet()
+    elif model_name == "GCN":
+        return TEST()#Seq_GCN()#TEST()#
+    elif model_name == "AxisNet":
+        return AxisNet(),PhysiologicalGenerator()
     else:
         log_warning("use implemented model")
         raise NotImplementedError("implement a custom model(%s) in /nets/models/" % model_name)
 
 
-def is_model_support(model_name, model_list):
+def is_model_support(model_name, model_list, log_flag):
     """
     :param model_name: model name
     :param model_list: implemented model list
     :return: model
     """
+    if log_flag:
+        print("========= model support check is model support() in" + os.path.basename(__file__))
     if not (model_name in model_list):
         log_warning("use implemented model")
         raise NotImplementedError("implement a custom model(%s) in /nets/models/" % model_name)
@@ -55,6 +73,8 @@ def summary(model, model_name):
         torchinfo.summary(model, (1, 3, 32, 128, 128))
     elif model_name in "PPNet":
         torchinfo.summary(model, (1, 1, 250))
+    elif model_name in "GCN":
+        torchinfo.summary(model,(32,3,32,32,32))
     else:
         log_warning("use implemented model")
         raise NotImplementedError("implement a custom model(%s) in /nets/models/" % model_name)
