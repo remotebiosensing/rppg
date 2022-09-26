a3cd5b463dceac4ec66d6087c99fda72fbb54cd2
dae yeol kim <kwelcomm@gmail.com>
2022-09-26 11:55:12 +0900

    * refactoring - _1_rppg_assesment.py.py

--- _1_rppg_assesment.py
+++ _1_rppg_assesment.py
@@ -1,4 +1,5 @@
 from pyVHR.analysis.testsuite import TestSuite
+
 cfgFilename = "./pyVHR/analysis/sample2.cfg"
 test = TestSuite(configFilename=cfgFilename)
 result = test.start(outFilename='result.h5', verb=1)
