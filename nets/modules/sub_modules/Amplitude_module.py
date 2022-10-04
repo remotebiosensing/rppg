3e288bb373bed66b6ffc30923c2aebec4cd5e62d
paperchae <forownsake@gmail.com>
2022-10-04 17:34:43 +0900

    leakyrelu -> elu

--- nets/modules/sub_modules/Amplitude_module.py
+++ nets/modules/sub_modules/Amplitude_module.py
@@ -5,31 +5,22 @@ class Amplitude_module(nn.Module):
     def __init__(self):
         super(Amplitude_module, self).__init__()
 
-        self.dbp = nn.Sequential(
+        self.dbp_linear = nn.Sequential(
             nn.Linear(100, 50),
             nn.Dropout1d(0.5),
             nn.Linear(50, 1),
-            nn.LeakyReLU()
+            nn.ELU()
 
         )
-        self.sbp = nn.Sequential(
+        self.sbp_linear = nn.Sequential(
             nn.Linear(100, 50),
             nn.Dropout1d(0.5),
             nn.Linear(50, 1),
-            nn.LeakyReLU()
+            nn.ELU()
         )
-        # self.dbp = nn.AdaptiveAvgPool1d(1)  # dbp
-        # self.sbp = nn.AdaptiveAvgPool1d(1)  # sbp
-        # self.amp = nn.AdaptiveAvgPool1d(1)  # amplitude of abp
-        # self.mbp = nn.Linear(360, 1)  # mbp
 
-    def forward(self, feature):
-        dbp = self.dbp(feature)
-        sbp = self.sbp(feature)
-        '''
-        to reduce load of model 
-        amp is set dependent variable of sbp
-        '''
-        # amp = self.amp(sbp)
+    def forward(self, d_out):
+        dbp = self.dbp_linear(d_out)
+        sbp = self.sbp_linear(d_out)
 
         return dbp, sbp
