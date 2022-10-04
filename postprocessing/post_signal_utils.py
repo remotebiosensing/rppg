377c777111acb1bdb4c5ef1bab5dc34fcdff27e0
paperchae <forownsake@gmail.com>
2022-10-04 17:42:58 +0900

    added scaler for postprocessing

--- postprocessing/post_signal_utils.py
+++ postprocessing/post_signal_utils.py
@@ -1,8 +1,14 @@
-import preprocessing.utils.signal_utils as su
+import numpy as np
+from sklearn import preprocessing
+
+def shape_scaler(input_sig):
+    input_sig = np.reshape(input_sig, (-1, 1))
+    scaled_output = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit_transform(input_sig)
+    return np.squeeze(scaled_output)
 
 
 def feature_combiner(corr, pred_s, pred_d):
-    scaled_corr = su.scaler(corr)
+    scaled_corr = shape_scaler(corr)
     amplitude = (pred_s.detach().cpu()[0] - pred_d.detach().cpu()[0]).numpy()
     height = pred_d.detach().cpu()[0].numpy()
 
