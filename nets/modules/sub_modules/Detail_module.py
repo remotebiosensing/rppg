a5605f1763898c62a27c21ce7e3a3f818ce865ba
paperchae <forownsake@gmail.com>
2022-10-04 17:37:13 +0900

    relu -> elu, added dropout, swapped order (d_module and t_module)

--- nets/modules/sub_modules/Detail_module.py
+++ nets/modules/sub_modules/Detail_module.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 class Detail_module_1D(nn.Module):
     def __init__(self, in_channels):
         super(Detail_module_1D, self).__init__()
-        self.in_channel = in_channels
         self.enconv = torch.nn.Sequential(
             nn.Conv1d(in_channels=in_channels, out_channels=32,
                       kernel_size=3, stride=1),
@@ -13,7 +12,7 @@ class Detail_module_1D(nn.Module):
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
             nn.BatchNorm1d(32),
-            nn.ReLU(),
+            nn.ELU(),
             # nn.MaxPool1d(2),
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
@@ -21,7 +20,8 @@ class Detail_module_1D(nn.Module):
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
             nn.BatchNorm1d(32),
-            nn.ReLU(),
+            nn.ELU(),
+            nn.Dropout1d(0.5),
             nn.MaxPool1d(2),
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
@@ -29,15 +29,15 @@ class Detail_module_1D(nn.Module):
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
             nn.BatchNorm1d(32),
-            nn.ReLU(),
-            # nn.MaxPool1d(2)
+            nn.ELU(),
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
             nn.BatchNorm1d(32),
             nn.Conv1d(in_channels=32, out_channels=32,
                       kernel_size=3, stride=1),
             nn.BatchNorm1d(32),
-            nn.ReLU(),
+            nn.ELU(),
+            nn.Dropout1d(0.5),
             nn.MaxPool1d(2)
         )
         self.deconv = torch.nn.Sequential(
@@ -58,21 +58,25 @@ class Detail_module_1D(nn.Module):
             nn.ConvTranspose1d(in_channels=16, out_channels=16,
                                kernel_size=3, stride=1),
             nn.BatchNorm1d(16),
-            nn.ConvTranspose1d(in_channels=16, out_channels=8,
+            nn.ConvTranspose1d(in_channels=16, out_channels=16,
                                kernel_size=3, stride=1),
-            nn.BatchNorm1d(8),
+            nn.BatchNorm1d(16),
             nn.ELU(),
-            nn.ConvTranspose1d(in_channels=8, out_channels=8,
+            nn.ConvTranspose1d(in_channels=16, out_channels=16,
                                kernel_size=3, stride=1),
-            nn.BatchNorm1d(8),
-            nn.ConvTranspose1d(in_channels=8, out_channels=1,
+            nn.BatchNorm1d(16),
+            nn.ConvTranspose1d(in_channels=16, out_channels=1,
                                kernel_size=3, stride=1),
             nn.BatchNorm1d(1),
             nn.ELU()
         )
+        self.relu = nn.ReLU()
+        self.elu = nn.ELU()
 
-    # TODO FORWARD안에 FEATURE 앞에다가 DATALOADER(__GETITEM__())에서 얻은 크기 정보 추가
-    def forward(self, ple_input):
-        at1 = self.enconv(ple_input)
-        at2 = self.deconv(at1)
-        return at1, at2
+    def forward(self, ple_input, at1, at2):
+        enout = self.enconv(ple_input)
+        out = self.elu(at1 + enout)
+        deout = self.deconv(out)
+        out = self.elu(at2 + deout)
+
+        return out
