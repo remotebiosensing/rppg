af6d1c56cb23b4254cbd47a13bcfbf719374967b
JunYeongNa <najy97@naver.com>
2022-10-14 15:04:12 +0900

    * add ETArPPGSubNet&Blocks

---
+++ nets/blocks/ETArPPGBlocks.py
@@ -0,0 +1,137 @@
+import torch
+
+class DepthwiseSeparableConv2d(torch.nn.Module):
+    def __init__(self, in_channels, out_channels=1):
+        super(DepthwiseSeparableConv2d, self).__init__()
+        # N, C, H, W
+        self.depthwise_conv = torch.nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3,
+                                              padding=1, groups=in_channels)
+        self.pointwise_conv = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # depthwise convolution
+        x = self.depthwise_conv(x)
+        # average pooling
+        x = torch.nn.functional.avg_pool2d(x, kernel_size=2)
+        # pointwise convolution
+        x = self.pointwise_conv(x)
+        # softmax
+        x = torch.nn.functional.softmax(x, dim=1)
+        return x
+
+
+class AttentionAndPooling(torch.nn.Module):
+    def __init__(self, output_shape):
+        super(AttentionAndPooling, self).__init__()
+        self.output_shape = output_shape
+
+    def forward(self, attmap, segment):
+        # attmap * segment
+        out = torch.mul(attmap, segment)
+        # (attmap * segment) + segment
+        out = torch.sum(out, segment)
+        # AdaptiveAvgPool2d
+        out = torch.nn.functional.adaptive_avg_pool2d(out, output_size=self.output_shape)
+        # adaptive avg pool2d
+        # Input : (N, C, H, W)
+        # Parameter : output_size(HxW) (tuple)
+        # Output : (N, C, output_shape)
+        return out
+
+
+class ETASubNetBlock(torch.nn.Module):
+    def __init__(self, output_shape):
+        super(ETASubNetBlock, self).__init__()
+        self.in_channels = 3
+        self.out_channels = 1
+        # define ETA-rPPGNet layers
+        self.DepthwiseSeparableConv2d = DepthwiseSeparableConv2d(self.in_channels, self.out_channels)
+        self.AttentionAndPooling = AttentionAndPooling(output_shape)
+
+    def forward(self, x):
+        # depth-wise separable convolution
+        attmap = self.DepthwiseSeparableConv2d(x)
+        # attention and pooling
+        x = self.AttentionAndPooling(attmap, x)
+        return x
+
+
+class Squeeze2(torch.nn.Module):
+    def __init__(self):
+        super(Squeeze2, self).__init__()
+
+    def forward(self, x):
+        x = torch.squeeze(x, dim=-1)
+        x = torch.squeeze(x, dim=-1)
+        return x
+
+
+class Unsqueeze2(torch.nn.Module):
+    def __init__(self):
+        super(Unsqueeze2, self).__init__()
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, dim=-1)
+        x = torch.unsqueeze(x, dim=-1)
+        return x
+
+
+class STBlock(torch.nn.Module):
+    def __init__(self):
+        super(STBlock, self).__init__()
+        self.conv1 = torch.nn.Conv3d(3, 3, kernel_size=(1, 3, 3))
+        self.conv2 = torch.nn.Conv3d(3, 3, kernel_size=(3, 1, 1))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # 3d average pooling
+        x = torch.nn.functional.avg_pool3d(x, kernel_size=(1, 2, 2))
+        return x
+
+
+class TimeDomainAttention(torch.nn.Module):
+    def __init__(self):
+        super(TimeDomainAttention, self).__init__()
+        self.gap3d = torch.nn.AdaptiveAvgPool3d(1)
+        self.squeeze2 = Squeeze2()
+        self.conv1d = torch.nn.Conv1d(in_channels=3, out_channels=3, kernel_size=5, padding='same')
+        self.unsqueeze2 = Unsqueeze2()
+        self.activation = torch.nn.Sigmoid()
+
+    def forward(self, d):
+        # Global Average Pooling : (N,C,H,W) -> (N,C,1,1)
+        m = self.gap3d(d)
+        # Squeeze : (N,C,1,1) -> (N,C)
+        m = self.squeeze2(m)
+        # one-dimensional convolution : (N,C) -> (N,C)
+        m = self.conv1d(m)
+        # 1 + sigmoid activation : (N,C) -> (N,C)
+        m = self.activation(m) + 1
+        # Unsqueeze : (N,C) -> (N,C,1,1)
+        m = self.unsqueeze2(m)
+        # multiplication : (N,C,1,1) * (N,C,H,W) -> (N,C,H,W)
+        d = m * d
+        return d
+
+
+class rPPGgenerator(torch.nn.Module):
+    def __init__(self, N, length):
+        super(rPPGgenerator, self).__init__()
+        self.length = length
+        self.N = N
+        self.AdaptiveAvgPool3d = torch.nn.AdaptiveAvgPool3d((N, 1, 1))
+        self.conv3d = torch.nn.Conv3d(1, 1, kernel_size=(1, 1, 1))
+
+    def forward(self, x):
+        # AdaptiveAvgPool3d
+        x = self.AdaptiveAvgPool3d(x)  # (N, C, 1, 1) -> (N, 1, 1)
+        # Conv3d
+        x = self.conv3d(x)  # (N, 1, 1) -> (N, 1, 1)
+        # Squeeze
+        x = torch.squeeze(x, dim=-1)  # (N, 1, 1) -> (N, 1)
+        # Linear interpolation
+        x = torch.nn.functional.interpolate(x, size=(self.length, 1), mode='linear')  # (N, 1) -> (length, 1)
+
+        return x
+
---
+++ nets/models/sub_models/ETArPPGSubNet.py
@@ -0,0 +1,34 @@
+# import pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from nets.blocks.blocks import ETASubNetBlock
+
+
+# define ETA-rPPGNet SubNet
+class ETArPPGNetSubNet(nn.Module):
+    def __init__(self, shape):
+        # segment shape : (C, t, W, H)
+        [self.c, self.t, self.h, self.w] = shape
+        super(ETArPPGNetSubNet, self).__init__()
+        self.h //= 2  # output height
+        self.w //= 2  # output width
+        self.ETASubNetBlock = ETASubNetBlock((self.h, self.w))
+
+    def forward(self, x):
+        # segment set shape : (N, C, t, W, H)
+        # segment shape : (C, t, W, H)
+        # feature maps before transpose : (N, C, H, W)
+        featuremap = torch.zeros((x.shape[0], self.c, self.h, self.w))
+
+        # transform segment to feature map
+        # x : C, t, W, H -> t, C, H, W for convolution
+        x = x.permute(1, 0, 3, 2)
+        for i in range(x.shape[0]):
+            # concat feature map
+            featuremap[i, :, :, :] = self.ETASubNetBlock(x[i])
+
+        # feature maps after transpose : (N, C, W, H)
+        featuremap = featuremap.permute(0, 1, 3, 2)
+        return featuremap
