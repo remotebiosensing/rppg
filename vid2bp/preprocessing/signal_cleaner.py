e3af1a9cccc3c5ee15c5da8d5733ef48587d3d74
najy97 <57242033+najy97@users.noreply.github.com>
2023-01-05 14:14:17 +0900

    * fix signal cleaner

--- vid2bp/preprocessing/signal_cleaner.py
+++ vid2bp/preprocessing/signal_cleaner.py
@@ -16,8 +16,13 @@ import wfdb
 import vid2bp.preprocessing.utils.signal_utils as su
 import vid2bp.preprocessing.utils.math_module as mm
 import vid2bp.preprocessing.mimic3temp as m3t
+from loss import sig_to_BPfiltersig as bpfilter
 
-"""Nan Detecting Modules"""
+"""unused"""
+
+
+def not_flat_signal_detector(target_signal):
+    return np.argwhere(np.diff(target_signal) != 0)
 
 
 def nan_detector(target_signal):
@@ -25,13 +30,8 @@ def nan_detector(target_signal):
     return nan_idx
 
 
-def not_nan_checker(target_signal):
-    return ~np.isnan(target_signal).any()
-
-
-def nan_checker(target_signal):
-    # return if signal has nan
-    return np.isnan(target_signal).any()
+def discrete_index_detector(target_index):
+    return np.where(np.diff(target_index) != 1)
 
 
 def not_nan_detector(target_signal):
@@ -39,8 +39,53 @@ def not_nan_detector(target_signal):
     return not_nan_idx
 
 
-def discrete_index_detector(target_index):
-    return np.where(np.diff(target_index) != 1)
+def signals_slice_by_flat(target_signals):
+    # input : signal list [signal1, signal2, ...]
+    return_signal_list = []
+    valid_flag = False
+    for target_signal in target_signals:
+        not_flat_index = not_flat_signal_detector(target_signal[0]).reshape(-1)
+        discrete_index = discrete_index_detector(not_flat_index)
+        if len(discrete_index[0]) == 0:
+            continue
+        else:
+            discrete_index = discrete_index[0].reshape(-1)
+        if discrete_index[0] != 0:
+            discrete_index = np.insert(discrete_index, 0, -1)
+        for i in range(len(discrete_index) - 1):
+            valid_flag = True
+            return_signal_list.append(
+                target_signal[:, not_flat_index[discrete_index[i] + 1]:not_flat_index[discrete_index[i + 1]]])
+    if valid_flag:
+        return True, return_signal_list
+    else:
+        return False, None
+
+
+def not_flat_signal_checker(target_signal, t=2, threshold=0.1, slicing=True):
+    # return True if not flat
+    if slicing:
+        target_signal = signal_slicer(target_signal, t=t, overlap=0)
+        for sliced_signal in target_signal:
+            if np.std(sliced_signal) < threshold:
+                return False
+    else:
+        if np.std(target_signal) < threshold:
+            return False
+    return True
+
+
+def not_nan_checker(target_signal):
+    return ~np.isnan(target_signal).any()
+
+
+def nan_interpolator(target_signal):
+    # not used
+    nan_idx = nan_detector(target_signal)
+    nan_idx = nan_idx.reshape(-1)
+    for idx in nan_idx:
+        target_signal[idx] = np.nanmean(target_signal)
+    return target_signal
 
 
 def signal_slice_by_nan(target_signal):
@@ -69,13 +114,12 @@ def nan_signal_slicer(target_signal):
         yield target_signal[idx[0]:idx[1]]
 
 
-def nan_interpolator(target_signal):
-    # not used
-    nan_idx = nan_detector(target_signal)
-    nan_idx = nan_idx.reshape(-1)
-    for idx in nan_idx:
-        target_signal[idx] = np.nanmean(target_signal)
-    return target_signal
+"""Nan Detecting Modules"""
+
+
+def nan_checker(target_signal):
+    # return if signal has nan
+    return np.isnan(target_signal).any(), len(np.where(np.isnan(target_signal))[0])
 
 
 """length checking modules"""
@@ -91,42 +135,24 @@ def length_checker(target_signal, length):
 """signal analysing modules"""
 
 
-def denoiser(target_signal, fs=125, order=5, cutoff=0.5):
-    b, a = signal.butter(order, cutoff, 'low', fs=fs)
-    filtered_signal = signal.filtfilt(b, a, target_signal)
-    return filtered_signal
+def peak_detector(target_signal, rol_sec, fs=125):
+    roll_mean = rolling_mean(target_signal, rol_sec, fs)
+    peak_heartpy = hp_peak.detect_peaks(target_signal, roll_mean, ma_perc=20, sample_rate=fs)
+    return peak_heartpy['peaklist']
 
 
-def peak_detector(target_signal, fs=125, threshold=0.5):
-    peak_idx = hp_peak.detect_peaks(target_signal, fs, threshold)
-    peak_idx = np.array(peak_idx)
-    return peak_idx
-
-
-def signal_shifter(target_signal, shift):
-    shifted_signal = np.roll(target_signal, shift)
-    return shifted_signal
+def bottom_detector(target_signal, rol_sec, fs=125):
+    target_signal = -target_signal
+    roll_mean = rolling_mean(target_signal, rol_sec, fs)
+    peak_heartpy = hp_peak.detect_peaks(target_signal, roll_mean, ma_perc=20, sample_rate=fs)
+    return peak_heartpy['peaklist']
 
 
 def correlation_checker(target_signal, reference_signal):
-    corr = np.correlate(target_signal, reference_signal, mode='full')
-    return corr
+    return np.abs(np.corrcoef(target_signal, reference_signal)[0, 1])
 
 
 """flag signal checking modules"""
-
-
-def not_flat_signal_checker(target_signal, t=2, threshold=0.1, slicing=True):
-    # return True if not flat
-    if slicing:
-        target_signal = signal_slicer(target_signal, t=t, overlap=0)
-        for sliced_signal in target_signal:
-            if np.std(sliced_signal) < threshold:
-                return False
-    else:
-        if np.std(target_signal) < threshold:
-            return False
-    return True
 
 
 def flat_signal_checker(target_signal, t=2, threshold=0.1, slicing=True):
@@ -142,40 +168,7 @@ def flat_signal_checker(target_signal, t=2, threshold=0.1, slicing=True):
     return False
 
 
-def not_flat_signal_detector(target_signal):
-    return np.argwhere(np.diff(target_signal) != 0)
-
-
-def signals_slice_by_flat(target_signals):
-    # input : signal list [signal1, signal2, ...]
-    return_signal_list = []
-    valid_flag = False
-    for target_signal in target_signals:
-        not_flat_index = not_flat_signal_detector(target_signal[0]).reshape(-1)
-        discrete_index = discrete_index_detector(not_flat_index)
-        if len(discrete_index[0]) == 0:
-            continue
-        else:
-            discrete_index = discrete_index[0].reshape(-1)
-        if discrete_index[0] != 0:
-            discrete_index = np.insert(discrete_index, 0, -1)
-        for i in range(len(discrete_index) - 1):
-            valid_flag = True
-            return_signal_list.append(
-                target_signal[:, not_flat_index[discrete_index[i] + 1]:not_flat_index[discrete_index[i + 1]]])
-    if valid_flag:
-        return True, return_signal_list
-    else:
-        return False, None
-
-
 """t second signal processing modules"""
-
-
-def signal_length_slicer(target_signal, length):
-    while length_checker(target_signal, length):
-        yield target_signal[:length]
-        target_signal = target_signal[length:]
 
 
 def signal_slicer(target_signal, fs=125, t=8, overlap=2):
@@ -183,7 +176,72 @@ def signal_slicer(target_signal, fs=125, t=8, overlap=2):
     while length_checker(target_signal, t * fs):
         return_signal_list.append(target_signal[:t * fs])
         target_signal = target_signal[(t - overlap) * fs:]
-    return return_signal_list
+    return np.array(return_signal_list)
+
+
+def signal_shifter(target_signal, gap):
+    shifted_signal = np.zeros_like(target_signal)
+    if gap > 0:
+        shifted_signal[:len(target_signal) - gap] = target_signal[gap:]
+        shifted_signal = shifted_signal[:-gap]
+    else:
+        shifted_signal[-gap:] = target_signal[:len(target_signal) + gap]
+        shifted_signal = shifted_signal[-gap:]
+    return shifted_signal
+
+
+def signal_matcher(abp_signal, ple_signal, abp_peaks, ple_peaks, threshold=0.8):
+    best_corr = 0
+    best_abp = []
+    best_ple = []
+    gaps = []
+    if len(abp_peaks) > len(ple_peaks):
+        for peak_index in range(len(ple_peaks)):
+            if peak_index - 1 > 0:
+                gaps.append(abp_peaks[peak_index - 1] - ple_peaks[peak_index])
+
+            gaps.append(abp_peaks[peak_index] - ple_peaks[peak_index])
+
+            if peak_index + 1 < len(abp_peaks):
+                gaps.append(abp_peaks[peak_index + 1] - ple_peaks[peak_index])
+        for gap in gaps:
+            matched_ple_signal = signal_shifter(ple_signal, gap)
+            if gap > 0:
+                matched_abp_signal = abp_signal[:-gap]
+            else:
+                matched_abp_signal = abp_signal[-gap:]
+            if length_checker(matched_abp_signal, 6 * 125) and length_checker(matched_ple_signal, 6 * 125):
+                correlation = correlation_checker(matched_abp_signal, matched_ple_signal)
+                if correlation > best_corr:
+                    best_corr = correlation
+                    best_abp = matched_abp_signal
+                    best_ple = matched_ple_signal
+    else:
+        for peak_index in range(len(abp_peaks)):
+            if peak_index - 1 > 0:
+                gaps.append(ple_peaks[peak_index - 1] - abp_peaks[peak_index])
+
+            gaps.append(ple_peaks[peak_index] - abp_peaks[peak_index])
+
+            if peak_index + 1 < len(ple_peaks):
+                gaps.append(ple_peaks[peak_index + 1] - abp_peaks[peak_index])
+
+        for gap in gaps:
+            matched_abp_signal = signal_shifter(abp_signal, gap)
+            if gap > 0:
+                matched_ple_signal = ple_signal[:-gap]
+            else:
+                matched_ple_signal = ple_signal[-gap:]
+            if length_checker(matched_abp_signal, 6 * 125) and length_checker(matched_ple_signal, 6 * 125):
+                correlation = correlation_checker(matched_abp_signal, matched_ple_signal)
+                if correlation > best_corr:
+                    best_corr = correlation
+                    best_abp = matched_abp_signal
+                    best_ple = matched_ple_signal
+    if best_corr < threshold:
+        return False, None, None, None
+    else:
+        return True, best_abp[:750], best_ple[:750], best_corr
 
 
 if __name__ == "__main__":
@@ -194,7 +252,11 @@ if __name__ == "__main__":
             if f.endswith('.hea') and ('_' in f) and ('layout' not in f):
                 segment_list.append(os.path.join(root_path, s, f))
     segment_list = [s[:-4] for s in segment_list]
-    for segment in tqdm(segment_list):
+    best_abp = []
+    best_ple = []
+    best_corr = []
+    total_piece = 0
+    for segment in tqdm(segment_list, desc='segment'):
         ple_idx, abp_idx = m3t.find_channel_idx(segment)
         ple, abp = np.squeeze(np.split(wfdb.rdrecord(segment, channels=[ple_idx, abp_idx]).p_signal, 2, axis=1))
 
@@ -202,36 +264,74 @@ if __name__ == "__main__":
 
         # slice signal by 8 seconds and overlap 2 seconds
         # ensure that the signal length is 8 seconds
-        sliced_abp = signal_slicer(abp)
-        sliced_ple = signal_slicer(ple)
+        fs = 125
+        t = 8
+        overlap = 2
+        sliced_abp = signal_slicer(abp, fs=fs, t=t, overlap=overlap)
+        sliced_ple = signal_slicer(ple, fs=fs, t=t, overlap=overlap)
+        total_piece += len(sliced_abp)
 
         # check if sliced signal is valid
         # if not, remove the signal
+        normal_abp = []
+        normal_ple = []
         for target_abp, target_ple in zip(sliced_abp, sliced_ple):
-            if nan_checker(target_abp) or nan_checker(target_ple)\
-                    or flat_signal_checker(target_abp) or flat_signal_checker(target_ple):
-                sliced_abp.remove(target_abp)
-                sliced_ple.remove(target_ple)
+            nan_flag_abp, num_nan_abp = nan_checker(target_abp)
+            nan_flag_ple, num_nan_ple = nan_checker(target_ple)
+            if nan_flag_abp or nan_flag_ple:
+                if num_nan_abp < 0.1 * len(target_abp) and num_nan_ple < 0.1 * len(target_ple):
+                    target_abp = nan_interpolator(target_abp)
+                    target_ple = nan_interpolator(target_ple)
+                else:
+                    continue
+            if flat_signal_checker(target_abp) or flat_signal_checker(target_ple):
+                continue
+            else:
+                normal_abp.append(target_abp)
+                normal_ple.append(target_ple)
 
+        if len(normal_abp) == 0 or len(normal_ple) == 0:
+            continue
         # denoise signal
-        denoised_abp = [denoiser(target_abp) for target_abp in sliced_abp]
-        denoised_ple = [denoiser(target_ple) for target_ple in sliced_ple]
+        lf = 0.5
+        hf = 8
+        # denoised_abp = normal_abp
+        # denoised_ple = normal_ple
+        # denoised_abp = [bpfilter(target_abp, t * fs, fs, lf, hf) for target_abp in normal_abp]
+        # denoised_ple = [bpfilter(target_ple, t * fs, fs, lf, hf) for target_ple in normal_ple]
+        denoised_abp = [filter_signal(target_abp, cutoff=hf, sample_rate=fs, order=2, filtertype='lowpass') for
+                        target_abp in normal_abp]
+        denoised_ple = [filter_signal(target_ple, cutoff=hf, sample_rate=fs, order=2, filtertype='lowpass') for
+                        target_ple in normal_ple]
+        # find peak index
+        rolling_sec = 1.5
+        peak_abp = [peak_detector(target_abp, rolling_sec, fs) for target_abp in normal_abp]
+        peak_ple = [peak_detector(target_ple, rolling_sec, fs) for target_ple in normal_ple]
 
-        # add index to ple, abp
-        ple = np.vstack([ple, np.arange(len(ple))])
-        abp = np.vstack([abp, np.arange(len(abp))])
-        # slice signal by nan
-        nan_flag_abp, abp_list = signal_slice_by_nan(abp)
-        nan_flag_ple, ple_list = signal_slice_by_nan(ple)
-        if nan_flag_abp and nan_flag_ple:
-            # check flat signal
-            flat_flag_abp, abp_list = signals_slice_by_flat(abp_list)
-            flat_flag_ple, ple_list = signals_slice_by_flat(ple_list)
-            if flat_flag_abp and flat_flag_ple:
-                # check signal length
-                # abp_list = [s for s in abp_list if length_checker(s, 6 * 125)]
-                # ple_list = [s for s in ple_list if length_checker(s, 6 * 125)]
-                for abp_idx in range(0, len(abp), 750):
-                    plt.plot(abp[0, abp_idx:abp_idx + 750])
-                    plt.show()
-                print(len(abp_list), len(ple_list))
+        # calculate correlation
+        # if correlation is less than threshold, remove the signal
+        threshold = 0.8
+
+        for target_abp, target_ple, target_peak_abp, target_peak_ple in zip(denoised_abp, denoised_ple, peak_abp,
+                                                                            peak_ple):
+            # check if peak is valid
+            if len(target_peak_abp) == 0 or len(target_peak_ple) == 0:
+                continue
+            # making...
+            match_flag, matched_abp, matched_ple, matched_corr = signal_matcher(target_abp, target_ple, target_peak_abp,
+                                                                                target_peak_ple, threshold)
+            if match_flag:
+                best_abp.append(matched_abp)
+                best_ple.append(matched_ple)
+                best_corr.append(matched_corr)
+    print('total matched piece: {}'.format(len(best_abp)))
+    print('total piece: {}'.format(total_piece))
+
+    for target_abp, target_ple, target_corr in zip(best_abp, best_ple, best_corr):
+        target_abp = (target_abp - np.min(target_abp)) / (np.max(target_abp) - np.min(target_abp))
+        target_ple = (target_ple - np.min(target_ple)) / (np.max(target_ple) - np.min(target_ple))
+        plt.title('correlation: {}'.format(target_corr))
+        plt.plot(target_abp)
+        plt.plot(target_ple)
+        plt.legend(['ABP', 'PLE'])
+        plt.show()
