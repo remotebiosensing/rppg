2f8ae9596d527f46726b01a17ad81e601913b3b3
paperchae <forownsake@gmail.com>
2023-02-06 11:31:33 +0900

    added patient's info to BPNet Custom dataset

--- vid2bp/BPNetDataset.py
+++ vid2bp/BPNetDataset.py
@@ -5,33 +5,30 @@ import matplotlib.pyplot as plt
 
 
 class BPNetDataset(Dataset):
-    def __init__(self, x_data, y_data, size_factor, device):
+    def __init__(self, x_data, y_data, size_factor, p_info, ohe, device):
         self.x_data = torch.FloatTensor(x_data).to(device, non_blocking=True)
-        # self.x_data = x_data
         self.y_data = torch.FloatTensor(y_data).to(device, non_blocking=True)
-        # self.y_data = y_data
         self.size = torch.FloatTensor(size_factor).to(device, non_blocking=True)
-        # self.size = size_factor
+        # self.size = torch.IntTensor(size_factor).to(device, non_blocking=True)
+        self.info = torch.FloatTensor(np.array(p_info, dtype=float)).to(device, non_blocking=True)
+        self.ohe = torch.FloatTensor(ohe).to(device, non_blocking=True)
         self.len = self.y_data.shape[0]
 
     def __getitem__(self, index):
         x = self.x_data[index]
-        # dx = torch.diff(x, dim=0)[89:269]
-        # torch.cat((torch.diff(x), torch.zeros(1).to('cuda:0')))
-        # ddx = torch.diff(torch.diff(x, dim=0), dim=0)[88:268]
-
         y = self.y_data[index]
-        dy = torch.diff(y, dim=0)[89:269]
-        ddy = torch.diff(torch.diff(y, dim=0), dim=0)[88:268]
         '''
         size[index][0] = np.min(diastolic list) 
         size[index][1] = np.max(systolic list) 
         '''
         d = self.size[index][0]
         s = self.size[index][1]
+        m = self.size[index][2]
+        info = self.info[index]
+        o = self.ohe[index]
         # m = self.size[index][2].to('cuda')
 
-        return x, y, dy, ddy, d, s
+        return x, y, d, s, m, info, o
 
     def __len__(self):
         return self.len
