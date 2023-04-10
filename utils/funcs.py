710a2c676e838b5482e38680da5727b6ad7b9f96
Dae Yeol Kim <spicyyeol@gmail.com>
2023-04-10 10:55:08 +0900

    * funcs update

--- utils/funcs.py
+++ utils/funcs.py
@@ -5,6 +5,8 @@ from scipy.signal import butter
 from scipy.sparse import spdiags
 import time
 import params as params
+from scipy.signal import find_peaks
+
 
 
 def detrend(signal, Lambda):
@@ -56,3 +58,18 @@ def plot_graph(start_point, length, target, inference):
 
 def normalize(input_val):
     return (input_val - np.mean(input_val)) / np.std(input_val)
+
+def _next_power_of_2(x):
+    """Calculate the nearest power of 2."""
+    return 1 if x == 0 else 2 ** (x - 1).bit_length()
+
+def calculate_hr(ppg_signal, fs=60, low_pass=0.75, high_pass=2.5):
+    """Calculate heart rate based on PPG using Fast Fourier transform (FFT)."""
+    ppg_signal = np.expand_dims(ppg_signal, 0)
+    N = _next_power_of_2(ppg_signal.shape[1])
+    f_ppg, pxx_ppg = scipy.signal.periodogram(ppg_signal, fs=fs, nfft=N, detrend=False)
+    fmask_ppg = np.argwhere((f_ppg >= low_pass) & (f_ppg <= high_pass))
+    mask_ppg = np.take(f_ppg, fmask_ppg)
+    mask_pxx = np.take(pxx_ppg, fmask_ppg)
+    fft_hr = np.take(mask_ppg, np.argmax(mask_pxx, 0))[0] * 60
+    return fft_hr
