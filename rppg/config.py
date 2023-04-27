8c1a604ebbea7140f3816c3698ce90dbfdd1f4d3
Dae Yeol Kim <spicyyeol@gmail.com>
2023-04-27 12:29:11 +0900

    refactoring * YAML to class

--- rppg/config.py
+++ rppg/config.py
@@ -1,8 +1,21 @@
 import yaml
 
+class CFG:
+    def __init__(self, yaml_path):
+        if isinstance(yaml_path, str):
+            with open(yaml_path, "r") as f:
+                yaml_data = yaml.safe_load(f)
+        else:
+            yaml_data = yaml_path
+
+        for key, value in yaml_data.items():
+            if isinstance(value, dict):
+                setattr(self, key, CFG(value))
+            else:
+                setattr(self, key, value)
+
 def get_config(file):
-    with open(file) as f:
-        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    cfg = CFG(file)
     return cfg
 
 if __name__ == '__main__':
