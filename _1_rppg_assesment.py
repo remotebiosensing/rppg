99c572d23d130e0cea3fa7a9c7ea1b76acf7c8e1
Dae Yeol Kim <spicyyeol@gmail.com>
2023-04-28 18:00:50 +0900

    remove comment

--- _1_rppg_assesment.py
+++
@@ -1,5 +0,0 @@
-from pyVHR.analysis.testsuite import TestSuite
-
-cfgFilename = "./pyVHR/analysis/sample2.cfg"
-test = TestSuite(configFilename=cfgFilename)
-result = test.start(outFilename='result.h5', verb=1)
