4e724bff7787b4888bc52dc7d9ac196010441eac
paperchae <forownsake@gmail.com>
2023-05-10 19:06:59 +0900

    unused files:removed/moved to other directories

--- vid2bp/stven/modules/sub_modules/skin_based_attention.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class skin_based_Attention(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size):
-        super(skin_based_Attention, self).__init__()
-        self.GAP = nn.AdaptiveAvgPool3d()
--- vid2bp/stven/preprocessing/MANHOB_HCIdataset.py
+++
@@ -1,6 +0,0 @@
-import pyedflib as edf
-import numpy as np
-
-path = '/home/paperc/PycharmProjects/dataset/manhob/'
-
-# file_name =
