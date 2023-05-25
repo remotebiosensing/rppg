98b03b39f203d187af22e4e1bf291e47c5b19627
paperchae <forownsake@gmail.com>
2023-05-25 09:48:22 +0900

    deleted unnecessary condition for data loader, added saving model feature to evaluate meta update rounds

--- examples/rppg/metaphys_mmpd.py
+++ examples/rppg/metaphys_mmpd.py
@@ -16,7 +16,7 @@ from rppg.MAML import MAML
 from rppg.run import test_fn
 from tqdm import tqdm
 import os
-
+import matplotlib.pyplot as plt
 SEED = 0
 
 # for Reproducible model
@@ -31,7 +31,8 @@ random.seed(SEED)
 generator = torch.Generator()
 generator.manual_seed(SEED)
 
-def test_fn(epoch, model, dataloaders, model_name, cal_type, metrics, wandb_flag: bool = True):
+
+def fn(epoch, model, dataloaders, model_name, cal_type, metrics, wandb_flag: bool = True):
     # To evaluate a model by subject, you can use the meta option
     step = "Test"
 
@@ -71,6 +72,19 @@ def test_fn(epoch, model, dataloaders, model_name, cal_type, metrics, wandb_flag
             if "Pearson" in metrics:
                 # print("Pearson", corr(hr_preds, hr_targets))
                 cost_list.append(corr(hr_preds, hr_targets)[0][1])
+            if np.isnan(cost_list).any() or (0.0 in cost_list):
+                # plt.title(str(cost_list))
+                # plt.plot(hr_preds, 'r', label='pred')
+                # plt.plot(hr_targets, 'b-.', label='target')
+                # plt.legend()
+                # plt.show()
+                # plt.close()
+                plt.title(str(cost_list))
+                plt.plot(target[0].detach().cpu().numpy(), 'r', label='target')
+                plt.plot(outputs[0].detach().cpu().numpy(), 'b-.', label='pred')
+                plt.legend()
+                plt.show()
+                plt.close()
         # cost_list.append(temp_list)
     return cost_list
 
@@ -84,22 +98,21 @@ if __name__ == "__main__":
         )
 
     # load dataset
-    if cfg.fit.meta.flag:
-        dataset = dataset_loader(
-            save_root_path=cfg.dataset_path,
-            model_name=cfg.fit.model,
-            dataset_name=[cfg.fit.train.dataset, cfg.fit.test.dataset],
-            time_length=cfg.fit.time_length,
-            overlap_interval=cfg.fit.overlap_interval,
-            img_size=cfg.fit.img_size,
-            train_flag=cfg.fit.train_flag,
-            eval_flag=cfg.fit.eval_flag,
-            meta=cfg.fit.meta.flag
-        )
+    dataset = dataset_loader(
+        save_root_path=cfg.dataset_path,
+        model_name=cfg.fit.model,
+        dataset_name=[cfg.fit.train.dataset, cfg.fit.test.dataset],
+        time_length=cfg.fit.time_length,
+        overlap_interval=cfg.fit.overlap_interval,
+        img_size=cfg.fit.img_size,
+        train_flag=cfg.fit.train_flag,
+        eval_flag=cfg.fit.eval_flag,
+        meta=cfg.fit.meta.flag
+    )
 
-        tasks = data_loader(datasets=dataset,
-                            batch_size=cfg.fit.train.batch_size,
-                            meta=cfg.fit.meta.flag)
+    tasks = data_loader(datasets=dataset,
+                        batch_size=cfg.fit.train.batch_size,
+                        meta=cfg.fit.meta.flag)
 
     model = get_model(
         model_name=cfg.fit.model,
@@ -108,7 +121,7 @@ if __name__ == "__main__":
         pretrained_path = cfg.model_path + cfg.fit.model + "_" + cfg.fit.meta.pretrain.dataset + ".pt"
         if os.path.isfile(pretrained_path):
             model.load_state_dict(torch.load(pretrained_path))
-            print("Load pre-trained model")
+            print("Loaded pre-trained model : {}".format(pretrained_path))
         else:
             print("No pre-trained model exist, start training")
             exec(open("configs/FIT_" + str(cfg.fit.model).upper() + "_UBFC_UBFC.yaml").read())
@@ -117,21 +130,21 @@ if __name__ == "__main__":
             model.load_state_dict(torch.load(pretrained_path))
     else:
         print("Cold start with no pre-trained model")
-    wandb_cfg = get_config("../../rppg/configs/WANDB_CONFG.yaml")
-
-    if wandb_cfg.flag:
-        wandb.init(project=wandb_cfg.wandb_project_name,
-                   entity=wandb_cfg.wandb_entity,
-                   name="Meta_"+cfg.fit.model + "/TRAIN_DATA:" +
-                        cfg.fit.train.dataset + "/TEST_DATA:" +
-                        cfg.fit.test.dataset + "/" +
-                        str(cfg.fit.time_length) + "/" +
-                        datetime.datetime.now().strftime('%m-%d%H:%M:%S'))
-        wandb.config = {
-            "learning_rate": cfg.fit.train.learning_rate,
-            "epochs": cfg.fit.train.epochs,
-            "batch_size": cfg.fit.batch_size
-        }
+    # wandb_cfg = get_config("../../rppg/configs/WANDB_CONFG.yaml")
+    #
+    # if wandb_cfg.flag:
+    #     wandb.init(project=wandb_cfg.wandb_project_name,
+    #                entity=wandb_cfg.wandb_entity,
+    #                name="Meta_" + cfg.fit.model + "/TRAIN_DATA:" +
+    #                     cfg.fit.train.dataset + "/TEST_DATA:" +
+    #                     cfg.fit.test.dataset + "/" +
+    #                     str(cfg.fit.time_length) + "/" +
+    #                     datetime.datetime.now().strftime('%m-%d%H:%M:%S'))
+    #     wandb.config = {
+    #         "learning_rate": cfg.fit.train.learning_rate,
+    #         "epochs": cfg.fit.meta.outer_update_num,
+    #         "batch_size": cfg.fit.batch_size
+    #     }
 
     if cfg.fit.meta.flag:
         meta = MAML(model=model,
@@ -141,34 +154,37 @@ if __name__ == "__main__":
                     outer_loss=cfg.fit.meta.outer_loss,
                     inner_lr=cfg.fit.meta.inner_lr,
                     outer_lr=cfg.fit.meta.outer_lr,
-                    num_updates=5)
+                    num_updates=cfg.fit.meta.inner_update_num)
 
         # cost_per_epoch = []
-        for epoch in range(cfg.fit.train.epochs):
-            meta.meta_update(tasks[:20], epoch)
-            cost_per_task = []
-            if cfg.fit.meta.fine_tune.flag:
-                for support, query in tasks[20:]:
-                    individual_model = meta.inner_update(support)
-                    cost_per_task.append(test_fn(epoch=epoch,
-                                                 model=individual_model,
-                                                 dataloaders=query,
-                                                 model_name=cfg.fit.model,
-                                                 cal_type=cfg.fit.test.cal_type,
-                                                 metrics=cfg.fit.test.metric,
-                                                 wandb_flag=False))
-
-                mean_cost = np.nan_to_num(np.array(cost_per_task)).mean(axis=0)
-                mae_cost_mean, rmse_cost_mean, mape_cost_mean, pearson_cost_mean = mean_cost[0], mean_cost[1], \
-                                                                                   mean_cost[2], mean_cost[3]
-                print("Epoch: {} | MAE: {:.4f} | RMSE: {:.4f} | MAPE: {:.4f} | Pearson: {:.4f}".format(epoch,
-                                                                                                       mae_cost_mean,
-                                                                                                       rmse_cost_mean,
-                                                                                                       mape_cost_mean,
-                                                                                                       pearson_cost_mean))
-                wandb.log({"MAE": mae_cost_mean,
-                           "RMSE": rmse_cost_mean,
-                           "MAPE": mape_cost_mean,
-                           "Pearson": pearson_cost_mean}, step=epoch)
-
+        for epoch in range(cfg.fit.meta.outer_update_num):
+            meta.meta_update(tasks, epoch)
+            # cost_per_task = []
+            # if cfg.fit.meta.fine_tune.flag:
+            #     for support, query in tasks[20:]:
+            #         individual_model = meta.inner_update(support)
+            #         cost_per_task.append(
+            #             fn(epoch=epoch, model=individual_model, dataloaders=query, model_name=cfg.fit.model,
+            #                cal_type=cfg.fit.test.cal_type, metrics=cfg.fit.test.metric, wandb_flag=False))
+            #
+            #     mean_cost = np.nan_to_num(np.array(cost_per_task)).mean(axis=0)
+            #     mae_cost_mean, rmse_cost_mean, mape_cost_mean, pearson_cost_mean = mean_cost[0], mean_cost[1], \
+            #                                                                        mean_cost[2], mean_cost[3]
+            #     print("Epoch: {} | MAE: {:.4f} | RMSE: {:.4f} | MAPE: {:.4f} | Pearson: {:.4f}".format(epoch,
+            #                                                                                            mae_cost_mean,
+            #                                                                                            rmse_cost_mean,
+            #                                                                                            mape_cost_mean,
+            #                                                                                            pearson_cost_mean))
+            #     if wandb_cfg.flag:
+            #         wandb.log({"MAE": mae_cost_mean,
+            #                    "RMSE": rmse_cost_mean,
+            #                    "MAPE": mape_cost_mean,
+            #                    "Pearson": pearson_cost_mean}, step=epoch)
+        torch.save(meta.model.state_dict(),
+                   cfg.model_path +
+                   "Meta_" +
+                   cfg.fit.model + "_" +
+                   cfg.fit.train.dataset +
+                   str(cfg.fit.meta.outer_update_num) + "_" +
+                   str(cfg.fit.meta.inner_update_num) + ".pt")
     print("END")
