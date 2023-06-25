587b4c06098ec59cfde0b406d79e3bb6bf2e070c
paperchae <forownsake@gmail.com>
2023-06-25 17:10:43 +0900

    Changed input parameter - n_clip -> time_length

--- rppg/nets/TSCAN.py
+++ rppg/nets/TSCAN.py
@@ -8,31 +8,31 @@ device = torch.device(
 )
 
 class TSM(nn.Module):
-    def __init__(self, n_clip=4, fold_div=3):
+    def __init__(self, time_length=180, fold_div=3):
         super().__init__()
-        self.n_clip = n_clip
+        self.time_length = time_length
         self.fold_div = fold_div
 
     def forward(self, input):
         B, C, H, W = input.shape
-        input = input.view(self.n_clip, -1, C, H, W)
+        input = input.view(-1, self.time_length, C, H, W)
 
         fold = C // self.fold_div
         last_fold = C - (self.fold_div - 1) * fold
 
         out1, out2, out3 = torch.split(input, [fold, fold, last_fold], dim=2)
 
-        up_out1 = torch.cat((torch.zeros((self.n_clip, 1, fold, H, W)).to(device), out1[:, 1:, :, :, :]), dim=1)
-        down_out2 = torch.cat((out2[:, :-1, :, :, :], torch.zeros((self.n_clip, 1, fold, H, W)).to(device)), dim=1)
+        up_out1 = torch.cat((torch.zeros((B//self.time_length, 1, fold, H, W)).to(device), out1[:, 1:, :, :, :]), dim=1)
+        down_out2 = torch.cat((out2[:, :-1, :, :, :], torch.zeros((B//self.time_length, 1, fold, H, W)).to(device)), dim=1)
         bidirection_out = torch.cat((up_out1, down_out2, out3), dim=2).view(B, C, H, W)
 
         return bidirection_out
 
 
 class MotionBranch(MotionModel):
-    def __init__(self, in_channels, out_channels, kernel_size, clip_n=4):
+    def __init__(self, in_channels, out_channels, kernel_size, time_length=180):
         super().__init__(in_channels, out_channels, kernel_size)
-        self.tsm = TSM(n_clip=clip_n, fold_div=3)
+        self.tsm = TSM(time_length=time_length, fold_div=3)
 
     def forward(self, inputs, mask1, mask2):
         m1 = torch.tanh(self.m_conv1(self.tsm(inputs)))
@@ -51,26 +51,26 @@ class MotionBranch(MotionModel):
 
 
 class TSCAN(nn.Module):
-    def __init__(self, clip_n=4):
+    def __init__(self, timelength=180):
         super(TSCAN, self).__init__()
         self.in_channels = 3
         self.out_channels = 32
         self.kernel_size = 3
-        self.clip_n = clip_n
+        self.time_length = timelength
         self.attention_mask1 = None
         self.attention_mask2 = None
 
         self.appearance_model = AppearanceModel(in_channels=self.in_channels, out_channels=self.out_channels,
                                                 kernel_size=self.kernel_size)
         self.motion_model = MotionBranch(in_channels=self.in_channels, out_channels=self.out_channels,
-                                         kernel_size=self.kernel_size, clip_n=self.clip_n)
+                                         kernel_size=self.kernel_size, time_length=self.time_length)
         self.linear_model = LinearModel(16384)
 
     def forward(self, inputs):
         averaged_frames = inputs[0]
         S, C, H, W = averaged_frames.shape
-        averaged_frames = averaged_frames.view(self.clip_n, -1, C, H, W)
-        averaged_frames = torch.mean(averaged_frames, dim=1, keepdim=True).expand(-1, S//self.clip_n, -1, -1, -1).reshape(S, C, H, W)
+        averaged_frames = averaged_frames.view(-1, self.time_length, C, H, W)
+        averaged_frames = torch.mean(averaged_frames, dim=1, keepdim=True).expand(-1, self.time_length, -1, -1, -1).reshape(S, C, H, W)
 
         self.attention_mask1, self.attention_mask2 = self.appearance_model(averaged_frames)
         motion_output = self.motion_model(inputs[1], self.attention_mask1, self.attention_mask2)
