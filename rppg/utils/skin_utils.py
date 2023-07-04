fbe6e63c8c84b7e68cbd349bdc59bc0f03f071a7
jh <munjonghyeon95@gmail.com>
2023-07-04 15:46:59 +0900

    Update skin_utils.py

--- rppg/utils/skin_utils.py
+++ rppg/utils/skin_utils.py
@@ -1,4 +1,3 @@
-
 import random
 from glob import glob
 
@@ -15,6 +14,7 @@ from colormath.color_diff import delta_e_cie2000
 from colormath.color_objects import sRGBColor, LabColor
 
 import matplotlib.pyplot as plt
+
 
 def getFace(frame):
     face_locations = face_recognition.face_locations(frame, 1, model='hog')
@@ -336,10 +336,47 @@ def avg(a, b):
     return [(int)((x + y) / 2) for x, y in zip(a, b)]
 
 
-# https://mellab-inha.github.io/static/pdf/CorrelationBetweenLightAbsorba.pdf
-categories_Fitzpatrick = ('#6E3B07', '#96734E',
-                          '#D7A306', '#EACD79',
-                          '#FBEED2', '#FEFEFE')
+def get_roi_skin_rgb(frame, image_size, skin_model, device):
+    # getFace (frame -> face)
+    face = getFace(frame)
+    face = cv2.resize(face, (image_size, image_size), interpolation=cv2.INTER_AREA)
+
+    # getSkin (face -> skin)
+    skin = getSkin(face, skin_model, device)
+
+    # getROI (face -> roi)
+    roi = getROI(face)
+
+    # getROI_RGB (skin, roi -> roi_RGB)
+    roi_RGB = getROI_RGB(skin, roi)
+
+    # getSkin_RGB (roiRGB -> skin_RGB)
+    skin_RGB = getSkin_RGB(roi_RGB)
+
+    # getGRGB (skin_RGB -> GRGB)
+    # GRGB = (skin_RGB[:, 1]/skin_RGB[:, 0]) + (skin_RGB[:, 1]/skin_RGB[:, 2])
+
+    return {'face': face, 'skin': skin, 'roi': roi, 'roi_RGB': roi_RGB, 'skin_RGB': skin_RGB}
+
+
+def plot_roi_skin_rgb(result_dict, show=False):
+    roi_img = np.zeros((result_dict['skin'].shape[0], result_dict['skin'].shape[1], 3), np.uint8)
+    for mask in result_dict['roi']:
+        roi_img = cv2.fillConvexPoly(roi_img, mask.astype(int), color=(255, 255, 255))
+    roi_img = cv2.bitwise_and(result_dict['skin'], roi_img)
+
+    RGB_img = np.ones((result_dict['skin'].shape[0], result_dict['skin'].shape[1], 3), np.uint8)
+    RGB_img = RGB_img * result_dict['skin_RGB'].round().astype(np.uint8)
+
+    debug_img_up = np.hstack([result_dict['face'], result_dict['skin']])
+    debug_img_down = np.hstack([roi_img, RGB_img])
+    debug_img = np.vstack([debug_img_up, debug_img_down])
+
+    plt.axis("off")
+    plt.imshow(debug_img)
+    plt.tight_layout()
+    if show:
+        plt.show()
 
 
 def hex2rgb(hex_str):
@@ -348,15 +385,201 @@ def hex2rgb(hex_str):
     return list(int(hex_str[i:i + 2], 16) for i in (0, 2, 4))
 
 
-def getSkinType(colors, categories):
+def hex2rgb_list(arr_hex):
+    return [hex2rgb(hex_str) for hex_str in arr_hex]
+
+
+def dominant_colors(image, n_clusters=3):
+    data = np.reshape(image, (-1, 3))  # shape > (W*H, C)
+    data = data[np.all(data != 0, axis=1)]  # 0을 포함한 RGB 픽셀 제거
+    data = np.float32(data)
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    flags = cv2.KMEANS_RANDOM_CENTERS
+    compactness, labels, colors = cv2.kmeans(data, n_clusters, None, criteria, 10, flags)
+    labels, counts = np.unique(labels, return_counts=True)
+
+    order = (-counts).argsort()
+    colors = colors[order]
+    counts = counts[order]
+
+    props = counts / counts.sum()
+
+    return colors, props
+
+
+def getSkinType(color, prop, categories):
     lab_labels = [convert_color(sRGBColor.new_from_rgb_hex(lbl), LabColor) for lbl in categories]
-    lab_colors = convert_color(sRGBColor(rgb_r=colors[0], rgb_g=colors[1], rgb_b=colors[2], is_upscaled=True), LabColor)
-    distances = [delta_e_cie2000(lab_colors, label) for label in lab_labels]
+    lab_color = convert_color(sRGBColor(rgb_r=color[0], rgb_g=color[1], rgb_b=color[2], is_upscaled=True), LabColor)
+    distances = [np.sum([delta_e_cie2000(lab_color, label) * prop]) for label in lab_labels]
     label_id = np.argmin(distances)
     distance: float = distances[label_id]
     category_hex = categories[label_id]
+
     return label_id, category_hex, distance
 
+
+def classifySkinType(skin_result_dict, categories, n_dominant_colors=3, debug=False):
+    colors, props = dominant_colors(skin_result_dict['skin'], n_clusters=n_dominant_colors)
+
+    # K-dominant-colors from K-mean algorithm (RGB)
+    hex_colors = ['#%02X%02X%02X' % tuple(np.around([r, g, b]).astype(int)) for r, g, b in colors]
+
+    # Skin Color chart/label
+    label_id, category_hex, _ = getSkinType(colors[0], props[0], categories)
+
+    dominant_rgb = hex2rgb_list(hex_colors)
+    # category_hex => RGB
+    skin_type_rgb = hex2rgb(category_hex)
+
+    result = {'dominant_rgb': dominant_rgb[0], 'skin_type_rgb': skin_type_rgb}
+
+    debug_img = None
+    if debug:
+        color_bars = []
+        color_w = skin_result_dict['face'].shape[1]
+        total_height = 0
+        for index, color in enumerate(colors):
+            # original_img : resized original image
+            color_h = round(skin_result_dict['face'].shape[0] * props[index])
+            total_height += color_h
+            bar = create_bar(color_h, color_w, color)
+            color_bars.append(bar)
+
+        padding_height = skin_result_dict['face'].shape[0] - total_height
+        if padding_height > 0:
+            padding = create_bar(padding_height, color_w, (255, 255, 255))
+            color_bars.append(padding)
+        color_bars = np.vstack(color_bars)[:skin_result_dict['face'].shape[0], :, :]
+
+        label_bars = []
+        label_h = color_w
+        label_w = (color_w * 3) // len(categories)
+        label_bgrs = []
+        for label_hex in categories:
+            hex_val = label_hex.lstrip('#')
+            lr, lg, lb = [int(hex_val[i:i + 2], 16) for i in (0, 2, 4)]
+            label_bgrs.append([lb, lg, lr])
+            label_bar = create_bar(label_h, label_w, [lb, lg, lr])
+            label_bars.append(label_bar)
+        padding_width = (color_w * 3) - (label_w * len(categories))
+        if padding_width > 0:
+            padding = create_bar(label_h, padding_width, (255, 255, 255))
+            label_bars.append(padding)
+        label_bars = np.hstack(label_bars)
+        label_bars = cv2.cvtColor(label_bars, cv2.COLOR_BGR2RGB)
+
+        debug_img = np.hstack([skin_result_dict['face'], skin_result_dict['skin'], color_bars])
+        debug_img = np.vstack([debug_img, label_bars])
+
+        label_y1 = label_h + 1
+        label_y2 = label_y1 + label_h - 4
+        label_x1 = label_id * label_w + 1
+        label_x2 = label_x1 + label_w - 2
+        debug_img = draw_rects(debug_img, [label_x1, label_y1, label_x2, label_y2], thickness=1)
+
+        color_y1 = 2
+        color_y2 = color_y1 + round(skin_result_dict['face'].shape[0] * props[0]) - 2
+        color_x1 = color_w * 2 + 2
+        color_x2 = color_x1 + color_w - 4
+        debug_img = draw_rects(debug_img, [color_x1, color_y1, color_x2, color_y2], thickness=1)
+
+    return result, debug_img
+
+
+def draw_rects(image, *rects, color=(255, 0, 0), thickness=2):
+    image = image.copy()
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(image, (x1 - 1, y1 - 1), (x2 + 1, y2 + 1), color, thickness)
+    return image
+
+
+def create_bar(height, width, color):
+    bar = np.zeros((height, width, 3), np.uint8)
+    bar[:] = color
+    return bar
+
+
+# https://symbiosisonlinepublishing.com/computer-science-technology/computerscience-information-technology06.php
+categories_HFFD = ('#2D221E',
+                   '#3C2E28',
+                   '#4B3932',
+                   '#5A453C',
+                   '#695046',
+                   '#785C50',
+                   '#87675A',
+                   '#967264',
+                   '#A57E6E',
+                   '#B48A78',
+                   '#C39582',
+                   '#D2A18C',
+                   '#E1AC96',
+                   '#F0B8A0',
+                   '#FFC3AA',
+                   '#FFCEB4',
+                   '#FFDABE',
+                   '#FFE5C8')
+
+# https://mellab-inha.github.io/static/pdf/CorrelationBetweenLightAbsorba.pdf
+categories_Fitzpatrick = ('#6E3B07',
+                          '#96734E',
+                          '#D7A306',
+                          '#EACD79',
+                          '#FBEED2',
+                          '#FEFEFE')
+
+# https://opencriticalcare.org/faq/how-is-skin-color-measured-for-the-purposes-of-oximeter-performance-validation/
+categories_VonLuschan = ('#1B1C2E',
+                         '#312529',
+                         '#402015',
+                         '#573229',
+                         '#603121',
+                         '#653020',
+                         '#643116',
+                         '#794D30',
+                         '#8E583E',
+                         '#9C6B43',
+                         '#BC9760',
+                         '#C7A464',
+                         '#DEB877',
+                         '#DFC17B',
+                         '#E1C16A',
+                         '#E3C467',
+                         '#EBD985',
+                         '#EBF19F',
+                         '#F2E297',
+                         '#F4D293',
+                         '#EFE2AD',
+                         '#F1E7C3',
+                         '#FFF9E1',
+                         '#FEF6E1',
+                         '#FCF8ED',
+                         '#FBFCF4',
+                         '#F4F1EA',
+                         '#F3EAE5',
+                         '#FAF0EF',
+                         '#FEF7E5',
+                         '#FDF6E6',
+                         '#FDFBE6',
+                         '#ECEBE9',
+                         '#F4F2F5')
+
+# https://skintone.google/
+categories_Monk = ('#f6ede4',
+                   '#f3e7db',
+                   '#f7ead0',
+                   '#eadaba',
+                   '#d7bd96',
+                   '#a07e56',
+                   '#825c43',
+                   '#604134',
+                   '#3a312a',
+                   '#292420')
+
+skinColorCharts = {'HFFD': categories_HFFD,
+                   'Fitzpatrick': categories_Fitzpatrick,
+                   'VonLuschan': categories_VonLuschan,
+                   'Monk': categories_Monk}
 
 if __name__ == "__main__":
     seed = 42
@@ -380,50 +603,26 @@ if __name__ == "__main__":
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cap.release()
 
-    # getFace (frame -> face)
-    face = getFace(frame)
+    # frame => {'face', 'skin', 'roi', 'roi_RGB', 'skin_RGB'}
+    result_dict = get_roi_skin_rgb(frame, image_size=128, skin_model=model, device=device)
 
-    img_size = 128
-    face = cv2.resize(face, (img_size, img_size), interpolation=cv2.INTER_AREA)
+    # Plot ROI-Skin-RGB debug image
+    plot_roi_skin_rgb(result_dict, show=True)
+    # plt.savefig(f'./test_dir/skin.png')
+    # plt.show()
 
-    # getSkin (face -> skin)
-    skin = getSkin(face, model, device)
+    # Skin Type Chart: Fitzpatrick, Monk, HFFD, VonLuschan
+    categories = skinColorCharts['Fitzpatrick']
 
-    # getROI (face -> roi)
-    roi = getROI(face)
-
-    # getROI_RGB (skin, roi -> roi_RGB)
-    roi_RGB = getROI_RGB(skin, roi)
-
-    # getSkin_RGB (roiRGB -> skin_RGB)
-    skin_RGB = getSkin_RGB(roi_RGB)
-
-    # getGRGB (skin_RGB -> GRGB)
-    # GRGB = (skin_RGB[:, 1]/skin_RGB[:, 0]) + (skin_RGB[:, 1]/skin_RGB[:, 2])
-
-    # getSkinType (skin_RGB -> SkinType)
-    # https://mellab-inha.github.io/static/pdf/CorrelationBetweenLightAbsorba.pdf
-    categories_Fitzpatrick = ('#6E3B07', '#96734E', '#D7A306', '#EACD79', '#FBEED2', '#FEFEFE')
-    category_id, category_hex, _ = getSkinType(skin_RGB, categories_Fitzpatrick)
+    # getSkinType from ROI-Skin-RGB
+    category_id, category_hex, _ = getSkinType(result_dict['skin_RGB'], prop=1., categories=categories)
     skin_type_rgb = hex2rgb(category_hex)
 
-    # Create debug_image
-    debug_img = True
-    if debug_img:
-        roi_img = np.zeros((skin.shape[0], skin.shape[1], 3), np.uint8)
-        for mask in roi:
-            roi_img = cv2.fillConvexPoly(roi_img, mask.astype(int), color=(255, 255, 255))
-        roi_img = cv2.bitwise_and(skin, roi_img)
+    # getSkinType from Skin Image
+    result, debug_img = classifySkinType(result_dict, categories=categories, debug=True)
 
-        RGB_img = np.ones((skin.shape[0], skin.shape[1], 3), np.uint8)
-        RGB_img = RGB_img * skin_RGB.round().astype(np.uint8)
-
-        debug_img_up = np.hstack([face, skin])
-        debug_img_down = np.hstack([roi_img, RGB_img])
-        debug_img = np.vstack([debug_img_up, debug_img_down])
-
-        plt.axis("off")
-        plt.imshow(debug_img)
-        plt.tight_layout()
-        # plt.savefig(f'./test_dir/skin.png')
-        plt.show()
+    plt.axis("off")
+    plt.imshow(debug_img)
+    plt.tight_layout()
+    # plt.savefig(f'./test_dir/s{s_num}.png')
+    plt.show()
