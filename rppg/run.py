4718609c5754997f88b2655d636a1b0bef10267c
paperchae <forownsake@gmail.com>
2023-07-31 17:55:59 +0900

    Simplified test_fn() parameters using cfg & added HRVAnalysis function

--- rppg/run.py
+++ rppg/run.py
@@ -2,11 +2,12 @@ import math
 import torch
 import wandb
 from tqdm import tqdm
-from rppg.utils.funcs import (get_hr, MAE, RMSE, MAPE, corr,SD, IrrelevantPowerRatio, normalize_torch)
-
+from rppg.utils.funcs import (get_hr, MAE, RMSE, MAPE, corr, SD, IrrelevantPowerRatio, normalize_torch)
+from rppg.utils.HRV_Analyze.hrv_utils import TimeHRVAnalysis, FrequencyHRVAnalysis, GraphicalReport
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+
 
 def run(model, sweep, optimizer, lr_sch, criterion, cfg, dataloaders):
     log = True
@@ -17,8 +18,11 @@ def run(model, sweep, optimizer, lr_sch, criterion, cfg, dataloaders):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     test_result = []
+    hr_predictions = []
+    train_cfg = cfg.fit.train
+    test_cfg = cfg.fit.test
     if cfg.fit.train_flag:
-        for epoch in range(cfg.fit.train.epochs):
+        for epoch in range(train_cfg.epochs):
             train_fn(epoch, model, optimizer, lr_sch, criterion, dataloaders[0], cfg.wandb.flag)
             val_loss = val_fn(epoch, model, criterion, dataloaders[1], cfg.wandb.flag)
             if best_loss > val_loss:
@@ -26,42 +30,36 @@ def run(model, sweep, optimizer, lr_sch, criterion, cfg, dataloaders):
                 eval_flag = True
                 if cfg.fit.model_save_flag:
                     torch.save(model.state_dict(), save_dir +
-                               "train" + cfg.fit.train.dataset +
-                               "_test" + cfg.fit.test.dataset +
+                               "train" + train_cfg.dataset +
+                               "_test" + test_cfg.dataset +
                                "_imgsize" + str(cfg.fit.img_size) +
                                ".pt")
             if log:
-                et = cfg.fit.test.eval_time_length if not sweep else cfg.fit.test.eval_time_length[2]
+                et = test_cfg.eval_time_length if not sweep else test_cfg.eval_time_length[2]
                 if cfg.fit.eval_flag and (eval_flag or (epoch + 1) % cfg.fit.eval_interval == 0):
-                    test_fn(epoch, model, dataloaders[2], vital_type=cfg.fit.test.vital_type,
-                            cal_type=cfg.fit.test.cal_type, bpf=cfg.fit.test.bpf, metrics=cfg.fit.test.metric,
-                            eval_time_length=et, wandb_flag=cfg.wandb.flag)
+                    test_fn(epoch, model, dataloaders[2], test_cfg=test_cfg, eval_time_length=et)
                 eval_flag = False
         if not sweep:
-            test_result = test_fn(0, model, dataloaders[2], vital_type=cfg.fit.test.vital_type,
-                                  cal_type=cfg.fit.test.cal_type, bpf=cfg.fit.test.bpf,
-                                  metrics=cfg.fit.test.metric, eval_time_length=cfg.fit.test.eval_time_length,
-                                  wandb_flag=cfg.wandb.flag)
+            test_result, _, _ = test_fn(0, model, dataloaders[2], test_cfg=test_cfg,
+                                        eval_time_length=cfg.fit.test.eval_time_length)
         else:
             for et in cfg.fit.test.eval_time_length:
-                test_result.append(test_fn(0, model, dataloaders[2], vital_type=cfg.fit.test.vital_type,
-                                           cal_type=cfg.fit.test.cal_type, bpf=cfg.fit.test.bpf,
-                                           metrics=cfg.fit.test.metric, eval_time_length=et,
-                                           wandb_flag=cfg.wandb.flag))
-    else:
-        # model = torch.load()
+                print("==========" + str(et) + "s==========")
+                test_result.append(test_fn(0, model, dataloaders[2], test_cfg=test_cfg, eval_time_length=et))
+    else:  # test only
         if not sweep:
-            test_result.append(test_fn(0, model, dataloaders[0], vital_type=cfg.fit.test.vital_type,
-                                       cal_type=cfg.fit.test.cal_type, bpf=cfg.fit.test.bpf,
-                                       metrics=cfg.fit.test.metric, eval_time_length=cfg.fit.test.eval_time_length,
-                                       wandb_flag=cfg.wandb.flag))
+            test_result.append(test_fn(0, model, dataloaders[0], test_cfg,
+                                       eval_time_length=cfg.fit.test.eval_time_length)[0])
         else:
             for et in cfg.fit.test.eval_time_length:
-                print("=========="+str(et)+"s==========")
-                test_result.append(test_fn(0, model, dataloaders[0], vital_type=cfg.fit.test.vital_type,
-                                           cal_type=cfg.fit.test.cal_type, bpf=cfg.fit.test.bpf,
-                                           metrics=cfg.fit.test.metric, eval_time_length=et,
-                                           wandb_flag=cfg.wandb.flag))
+                print("==========" + str(et) + "s==========")
+                result, hr_prediction, hr_target = test_fn(0, model, dataloaders[0], test_cfg, eval_time_length=et)
+                test_result.append(result)
+                hr_predictions.append(hr_prediction)
+                # test_result.append(test_fn(0, model, dataloaders[0], vital_type=cfg.fit.test.vital_type,
+                #                            cal_type=cfg.fit.test.cal_type, bpf=cfg.fit.test.bpf,
+                #                            metrics=cfg.fit.test.metric, eval_time_length=et,
+                #                            wandb_flag=cfg.wandb.flag))
 
     return test_result
 
@@ -140,7 +138,7 @@ def val_fn(epoch, model, criterion, dataloaders, wandb_flag: bool = True):
         return running_loss / tepoch.__len__()
 
 
-def test_fn(epoch, model, dataloaders, vital_type, cal_type, bpf, metrics, eval_time_length=10, wandb_flag: bool = False):
+def test_fn(epoch, model, dataloaders, test_cfg, eval_time_length=10):
     # To evaluate a model by subject, you can use the meta option
     step = "Test"
     model_name = model.__module__.split('.')[-1]
@@ -179,26 +177,38 @@ def test_fn(epoch, model, dataloaders, vital_type, cal_type, bpf, metrics, eval_
     pred_chunks = torch.stack(list(torch.split(empty_tensor[1:].detach(), interval))[:-1], dim=0)
     target_chunks = torch.stack(list(torch.split(empty_tensor2[1:].detach(), interval))[:-1], dim=0)
 
-    hr_pred, hr_target = get_hr(pred_chunks, target_chunks, model_type=model_type, vital_type=vital_type,
-                                cal_type=cal_type, fs=fs, bpf=bpf)
-
+    hr_pred, hr_target = get_hr(pred_chunks, target_chunks, model_type=model_type, vital_type=test_cfg.vital_type,
+                                cal_type=test_cfg.cal_type, fs=test_cfg.fs, bpf=test_cfg.bpf,
+                                report_flag=test_cfg.report_flag)
+    # gr(hr_pred, hr_target).bland_altman_plot(title='Bland-Altman Plot',
+    #                                          xlabel='Average of the two methods',
+    #                                          ylabel='Difference between the two methods')
     hr_pred = np.asarray(hr_pred.detach().cpu())
     hr_target = np.asarray(hr_target.detach().cpu())
 
     test_result = []
-    if "MAE" in metrics:
+    if "MAE" in test_cfg.metric:
         test_result.append(round(MAE(hr_pred, hr_target), 3))
         print("MAE", MAE(hr_pred, hr_target))
-    if "RMSE" in metrics:
+    if "RMSE" in test_cfg.metric:
         test_result.append(round(RMSE(hr_pred, hr_target), 3))
         print("RMSE", RMSE(hr_pred, hr_target))
-    if "MAPE" in metrics:
+    if "MAPE" in test_cfg.metric:
         test_result.append(round(MAPE(hr_pred, hr_target), 3))
         print("MAPE", MAPE(hr_pred, hr_target))
-    if "Pearson" in metrics:
+    if "Pearson" in test_cfg.metric:
         test_result.append(round(corr(hr_pred, hr_target)[0][1], 3))
         print("Pearson", corr(hr_pred, hr_target))
-    return test_result
+    if test_cfg.report_flag:
+        if test_cfg.cal_type == 'PEAK':
+            # time_hrv_pred = TimeHRVAnalysis(hr_pred, fs=test_cfg.fs)
+            time_hrv_target = TimeHRVAnalysis(hr_target, fs=test_cfg.fs)
+            # time_hrv_pred.report()
+            # time_hrv_target.report()
+        elif test_cfg.cal_type == 'FFT':
+            freq_hrv_pred = FrequencyHRVAnalysis(hr_pred, fs=test_cfg.fs)
+        print('break_point')
+    return test_result, hr_pred, hr_target
 
 
 def find_lr(model, train_loader, optimizer, criterion, init_value=1e-8, final_value=10., beta=0.98):
